53237089493ba82cdf5399c42f19dca231bd7c96
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2013-04-05 21:14:37 -0400

    Aln.convert.pl

---
+++ Scripts/Aln.convert.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use Bio::AlignIO;
+
+my($iformat,$oformat) = @ARGV;
+($iformat and $oformat) or die "
+Usage:
+   $0 in-format out-format < in_file > output_file
+
+   in-format	Input file's format.
+   out-format	Output file's format.
+   in_file	Input file.
+   out_file	Output file.
+
+Example:
+   # Re-format example.fa into Stockholm
+   $o fasta stockholm < example.fa > example.stk
+
+Supported formats are:
+   bl2seq, clustalw, emboss, fasta, maf, mase, mega,
+   meme, metafasta, msf, nexus, pfam, phylip, po,
+   prodom, psi, selex, stockholm, XMFA, arp
+
+";
+
+$in  = Bio::AlignIO->new(-fh => \*STDIN, -format => $iformat);
+$out = Bio::AlignIO->new(-fh => \*STDOUT, -format => $oformat);
+while ( my $aln = $in->next_aln ) { $out->write_aln($aln) }
+
