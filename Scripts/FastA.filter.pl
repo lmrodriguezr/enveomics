61fa2abf84c28f67e59a71514e6dda44c647ad02
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2013-04-09 19:21:48 -0400

    FastA.filter.pl updated
    Extra options allowed: -r, -q, -h.

--- Scripts/FastA.filter.pl
+++ Scripts/FastA.filter.pl
@@ -1,38 +1,51 @@
 #!/usr/bin/perl
 #
 # @author: Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
-# @update: Jan 09 2013
+# @update: Apr 09 2013
 # @license: artistic license 2.0
 #
 
 use warnings;
 use strict;
+use Getopt::Std;
 
-my($list, $fa) = @ARGV;
-
-($list and $fa) or die "
+sub HELP_MESSAGE { die "
 .Description:
    Extracts a subset of sequences from a FastA file.
 
-.Usage: $0 list.txt seqs.fa > subset.fa
+.Usage: $0 [options] list.txt seqs.fa > subset.fa
    
+   [options]
+   -r		Reverse list.  Extracts sequences NOT present in the list.
+   -q		Runs quietly.
+   -h		Prints this message and exits.
+
+   [mandatory]
    list.txt	List of sequences to extract.
    seqs.fa	FastA file containing the superset of sequences.
    subset.fa	FastA file to be created.
 
-";
+" }
 
+my %o=();
+getopts('rhq', \%o);
+my($list, $fa) = @ARGV;
+($list and $fa) or &HELP_MESSAGE;
+$o{h} and &HELP_MESSAGE;
+
+print STDERR "Reading list.\n" unless $o{q};
 open LI, "<", $list or die "Cannot read file: $list: $!\n";
 my %li = map { chomp; $_ => 1 } <LI>;
 close LI;
 
+print STDERR "Filtering FastA.\n" unless $o{q};
 open FA, "<", $fa or die "Cannot read file: $fa: $!\n";
 my $good = 0;
 while(my $ln = <FA>){
    chomp $ln;
    if($ln =~ m/^>((\S+).*)/){ $good = (exists $li{$1} or exists $li{">$1"} or exists $li{$2} or exists $li{$ln}) }
-   elsif($ln =~ m/^>/){ $good=0; warn "Non-cannonical defline, line $.: $ln\n" }
-   print "$ln\n" if $good;
+   elsif($ln =~ m/^>/){ $good=0; print STDERR "Warning: Non-cannonical defline, line $.: $ln\n" }
+   print "$ln\n" if (($good and not $o{r}) or ($o{r} and not $good));
 }
 close FA;
 
