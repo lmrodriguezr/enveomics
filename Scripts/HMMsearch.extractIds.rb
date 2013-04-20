035c4811b11b6507c376e6395fa40cf2a6c51fa4
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2013-04-20 18:33:15 -0400

    Update HMMsearch.extractIds.rb

--- Scripts/HMMsearch.extractIds.rb
+++ Scripts/HMMsearch.extractIds.rb
@@ -17,10 +17,10 @@ Extracts the sequence IDs and query model form a (multiple) HMMsearch report (fo
 Usage: #{$0} [options] < input.hmmsearch > list.txt"
    opts.separator ""
    opts.separator "Options"
-   opts.on("-E", "--all-evalue FLOAT", "Maximum e-value of sequence to report result."){|v| o[:all_evalue]=v }
-   opts.on("-S", "--all-score FLOAT", "Minimum score of sequence to report result."){|v| o[:all_score]=v }
-   opts.on("-e", "--best-evalue FLOAT", "Maximum e-value of best domain to report result."){|v| o[:best_evalue]=v }
-   opts.on("-s", "--best-score FLOAT", "Minimum score of best domain to report result."){|v| o[:best_score]=v }
+   opts.on("-E", "--all-evalue FLOAT", "Maximum e-value of sequence to report result."){|v| o[:all_evalue]=v.to_f }
+   opts.on("-S", "--all-score FLOAT", "Minimum score of sequence to report result."){|v| o[:all_score]=v.to_f }
+   opts.on("-e", "--best-evalue FLOAT", "Maximum e-value of best domain to report result."){|v| o[:best_evalue]=v.to_f }
+   opts.on("-s", "--best-score FLOAT", "Minimum score of best domain to report result."){|v| o[:best_score]=v.to_f }
    opts.on("-n", "--no-model", "Do not include the query model in the output list."){ o[:model]=FALSE }
    opts.on("-q", "--quiet", "Run quietly."){ o[:quiet]=TRUE }
    opts.on("-h", "--help", "Display this screen.") do
@@ -39,16 +39,19 @@ ARGF.each_line do |ln|
    when :header
       qm = /Query:\s+(.*?)\s+/.match(ln)
       qm.nil? or query=qm[1]
-      at = :list unless /^[\-\s]+$/.match(ln).nil?
+      unless /^[\-\s]+$/.match(ln).nil?
+         at = :list
+	 STDERR.puts "Parsing hits against #{query}" unless o[:quiet]
+      end
    when :list
       if /^\s*$/.match(ln).nil?
          row = ln.split(/\s+/)
 	 raise "Unable to parse seemingly malformed list of hits in line #{$.}:\n#{ln}" unless row.length==11
 	 good = TRUE
-	 good &&= ( o[:all_evalue].nil? || row[1]<=o[:all_evalue] )
-	 good &&= ( o[:all_score].nil? || row[2]>=o[:all_score] )
-	 good &&= ( o[:best_evalue].nil? || row[4]<=o[:best_evalue] )
-	 good &&= ( o[:best_score].nil? || row[5]>=o[:best_score] )
+	 good &&= ( o[:all_evalue].nil? || row[1].to_f <= o[:all_evalue] )
+	 good &&= ( o[:all_score].nil? || row[2].to_f >= o[:all_score] )
+	 good &&= ( o[:best_evalue].nil? || row[4].to_f <= o[:best_evalue] )
+	 good &&= ( o[:best_score].nil? || row[5].to_f >= o[:best_score] )
 	 good and puts row[9]+(o[:model]?"\t#{query}":"")+"\n"
       else
          at = :align
