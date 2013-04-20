6bdc44824a98ad9a22794fb69428abea3464b70f
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2013-04-20 18:46:37 -0400

    Update HMMsearch.extractIds.rb

--- Scripts/HMMsearch.extractIds.rb
+++ Scripts/HMMsearch.extractIds.rb
@@ -17,12 +17,12 @@ Extracts the sequence IDs and query model form a (multiple) HMMsearch report (fo
 Usage: #{$0} [options] < input.hmmsearch > list.txt"
    opts.separator ""
    opts.separator "Options"
-   opts.on("-E", "--all-evalue FLOAT", "Maximum e-value of sequence to report result."){|v| o[:all_evalue]=v.to_f }
-   opts.on("-S", "--all-score FLOAT", "Minimum score of sequence to report result."){|v| o[:all_score]=v.to_f }
-   opts.on("-e", "--best-evalue FLOAT", "Maximum e-value of best domain to report result."){|v| o[:best_evalue]=v.to_f }
-   opts.on("-s", "--best-score FLOAT", "Minimum score of best domain to report result."){|v| o[:best_score]=v.to_f }
+   opts.on("-E", "--all-evalue FLOAT", "Maximum e-value of sequence to report result."){|v| o[:all_evalue] = v.to_f }
+   opts.on("-S", "--all-score FLOAT", "Minimum score of sequence to report result."){|v| o[:all_score] = v.to_f }
+   opts.on("-e", "--best-evalue FLOAT", "Maximum e-value of best domain to report result."){|v| o[:best_evalue] = v.to_f }
+   opts.on("-s", "--best-score FLOAT", "Minimum score of best domain to report result."){|v| o[:best_score] = v.to_f }
    opts.on("-n", "--no-model", "Do not include the query model in the output list."){ o[:model]=FALSE }
-   opts.on("-q", "--quiet", "Run quietly."){ o[:quiet]=TRUE }
+   opts.on("-q", "--quiet", "Run quietly."){ o[:quiet] = TRUE }
    opts.on("-h", "--help", "Display this screen.") do
       puts opts
       exit
@@ -32,6 +32,7 @@ end.parse!
 
 at = :header
 query = ""
+i = 0
 ARGF.each_line do |ln|
    next unless /^(#.*)$/.match(ln).nil?
    ln.chomp!
@@ -41,6 +42,7 @@ ARGF.each_line do |ln|
       qm.nil? or query=qm[1]
       unless /^[\-\s]+$/.match(ln).nil?
          at = :list
+	 i  = 0
 	 STDERR.puts "Parsing hits against #{query}" unless o[:quiet]
       end
    when :list
@@ -52,9 +54,13 @@ ARGF.each_line do |ln|
 	 good &&= ( o[:all_score].nil? || row[2].to_f >= o[:all_score] )
 	 good &&= ( o[:best_evalue].nil? || row[4].to_f <= o[:best_evalue] )
 	 good &&= ( o[:best_score].nil? || row[5].to_f >= o[:best_score] )
-	 good and puts "#{row[9]}#{ o[:model] ? "\t#{query}" : "" }\n"
+	 if good
+	    puts(row[9] + (o[:model]?"\t#{query}":"") + "\n")
+	    i+=1
+	 end
       else
          at = :align
+         STDERR.puts "  #{i} results." unless o[:quiet]
       end
    when :align
       at = :header unless /^\/\/$/.match(ln).nil?
