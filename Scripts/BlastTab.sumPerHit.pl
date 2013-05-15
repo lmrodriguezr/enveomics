4ccca0e876364f9350c7516c176f2775699a8ebb
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2013-05-15 01:40:23 -0400

    New features on BlastTab.sumPerHits.pl
    The -w argument is now optional, and the -z argument was added.

--- Scripts/BlastTab.sumPerHit.pl
+++ Scripts/BlastTab.sumPerHit.pl
@@ -2,7 +2,7 @@
 
 #
 # @author: Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
-# @update: Apr-08-2013
+# @update: May-15-2013
 # @license: artistic license 2.0
 #
 
@@ -24,31 +24,40 @@ die "
    blast... *	One or more BLAST files.
    out-file	A two-columns tab-delimited file containing the summed weights per hit.
    
-   -w <str> *	Weights file: A two-columns tab-delimited file containing the weight of each query.
+   -w <str>	Weights file: A two-columns tab-delimited file containing the weight of each query.
    -s <float>	Minimum score.  By default: 0.
    -i <float>	Minimum identity (in percentage).  By default: 0.
    -m <int>	Maximum number of queries.  Set to 0 for all.  By default: 0.
    -n		Normalize weights by the number of hits per query.
+   -z		Add zero when weight is not found (by default: doesn't list them).
    -q		Run quietly.
    -h		Display this message and exit.
 
    * Mandatory
 
+.Note:
+   The weights (-w parameter) are optional, but its use is encouraged.  When weights are not passed,
+   the script simply assumes all queries to be equally weighted (unity), a result that can be faster
+   to compute with, for example:
+      cat blast... | awk '{print \$2}' | sort | uniq -c | awk '{print \$2\"\\t\"\$1}' > out-file
+   It is equivalent to simply count the number of times that each subject occurs.
 "
 }
 
 my %o = ();
-getopts('w:s:i:m:nqh', \%o);
+getopts('w:s:i:m:znqh', \%o);
 $o{h} and &HELP_MESSAGE;
-$o{w} or &HELP_MESSAGE;
 $o{s}||=0;
 $o{i}||=0;
 $o{m}||=0;
 
-print STDERR "Reading counts.\n" unless $o{q};
-open COUNT, "<", $o{w} or die "Cannot open file: $o{w}: $!\n";
-my %count = map {split /\t/} <COUNT>;
-close COUNT;
+my %count;
+if($o{w}){
+   print STDERR "Reading counts.\n" unless $o{q};
+   open COUNT, "<", $o{w} or die "Cannot open file: $o{w}: $!\n";
+   %count = map {split /\t/} <COUNT>;
+   close COUNT;
+}
 
 print STDERR "Reading BLASTs.\n" unless $o{q};
 my $qry = '';
@@ -69,7 +78,13 @@ BFILE: for my $blast (@ARGV){
       next if ($o{s} and $ln[11]<$o{s}) or ($o{i} and $ln[2]<$o{i});
       unless(exists $count{$ln[0]}){
 	 $noQry++;
-	 next;
+	 if(not $o{w}){
+	    $count{$ln[0]}=1;
+	 }elsif($o{z}){
+	    $count{$ln[0]}=0;
+	 }else{
+	    next BLINE;
+	 }
       }
       
       if($qry ne $ln[0]){
@@ -87,7 +102,7 @@ BFILE: for my $blast (@ARGV){
    ($out{$_->[0]}||=0) += ($_->[1]/($o{n}?$hits:1)) for @buf;
    close BLAST;
 }
-print STDERR "Warning: Couldn't find $noQry queries\n" if $noQry;
+print STDERR "Warning: Couldn't find $noQry queries\n" if $noQry and $o{w};
 
 for my $h (keys %out){
    print "$h\t".$out{$h}."\n";
