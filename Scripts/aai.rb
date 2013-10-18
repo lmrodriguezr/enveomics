5e28d379c0dcdb32e281d8e3dedc44b31bd32cf8
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2013-10-18 14:46:48 -0400

    New AAI Script
    aai.rb created and ani.rb now supports BLAT.

--- Scripts/aai.rb
+++ Scripts/aai.rb
@@ -16,27 +16,26 @@ rescue LoadError
    has_rest_client = FALSE
 end
 
-o = {:win=>1000, :step=>200, :len=>700, :id=>70, :hits=>50, :q=>FALSE, :bin=>'', :program=>'blast+', :thr=>1}
+o = {:len=>0, :id=>70, :bits=>0, :hits=>50, :q=>FALSE, :bin=>'', :program=>'blast+', :thr=>1}
 OptionParser.new do |opts|
    opts.banner = "
-Calculates the Average Nucleotide Identity between two genomes.
+Calculates the Average Amino acid Identity between two genomes.
 
 Usage: #{$0} [options]"
    opts.separator ""
    opts.separator "Mandatory"
-   opts.on("-1", "--seq1 FILE", "Path to the FastA file containing the genome 1."){ |v| o[:seq1] = v }
-   opts.on("-2", "--seq2 FILE", "Path to the FastA file containing the genome 2."){ |v| o[:seq2] = v }
+   opts.on("-1", "--seq1 FILE", "Path to the FastA file containing the genome 1 (proteins)."){ |v| o[:seq1] = v }
+   opts.on("-2", "--seq2 FILE", "Path to the FastA file containing the genome 2 (proteins)."){ |v| o[:seq2] = v }
    if has_rest_client
-      opts.separator "    Alternatively, you can supply a GI with the format gi:12345 instead of files."
+      opts.separator "    Alternatively, you can supply the GI of a genome (nucleotides) with the format gi:12345 instead of files."
    else
       opts.separator "    Install rest-client to enable gi support."
    end
    opts.separator ""
    opts.separator "Search Options"
-   opts.on("-w", "--win INT", "Window size in the ANI calculation (in bp).  By default: #{o[:win].to_s}."){ |v| o[:win] = v.to_i }
-   opts.on("-s", "--step INT", "Step size in the ANI calculation (in bp).  By default: #{o[:step].to_s}."){ |v| o[:step] = v.to_i }
-   opts.on("-l", "--len INT", "Minimum alignment length (in bp).  By default: #{o[:len].to_s}."){ |v| o[:len] = v.to_i }
+   opts.on("-l", "--len INT", "Minimum alignment length (in aa).  By default: #{o[:len].to_s}."){ |v| o[:len] = v.to_i }
    opts.on("-i", "--id NUM", "Minimum alignment identity (in %).  By default: #{o[:id].to_s}."){ |v| o[:id] = v.to_f }
+   opts.on("-s", "--bitscore NUM", "Minimum bit score (in bits).  By default: #{o[:bits].to_s}."){ |v| o[:bits] = v.to_f }
    opts.on("-n", "--hits INT", "Minimum number of hits.  By default: #{o[:hits].to_s}."){ |v| o[:hits] = v.to_i }
    opts.separator ""
    opts.separator "Software Options"
@@ -56,7 +55,6 @@ Usage: #{$0} [options]"
 end.parse!
 abort "-1 is mandatory" if o[:seq1].nil?
 abort "-2 is mandatory" if o[:seq2].nil?
-abort "Step size must be smaller than window size." if o[:step] > o[:win]
 o[:bin] = o[:bin]+"/" if o[:bin].size > 0
 
 Dir.mktmpdir do |dir|
@@ -68,46 +66,47 @@ Dir.mktmpdir do |dir|
       gi = /^gi:(\d+)/.match(o[seq])
       if not gi.nil?
 	 abort "GI requested but rest-client not supported.  First install gem rest-client." unless has_rest_client
-	 response = RestClient.get 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi', {:params=>{:db=>'nuccore', :rettype=>'fasta', :id=>gi[1]}}
-	 abort "Unable to reach NCBI EUtils, error code #{response.code}." unless response.code == 200
+	 $stderr.puts "  Downloading dataset from GI:#{gi[1]}." unless o[:q]
+	 responseLink = RestClient.get 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi', {:params=>{:db=>'protein',:dbfrom=>'nuccore', :id=>gi[1]}}
+	 abort "Unable to reach NCBI EUtils, error code #{responseLink.code}." unless responseLink.code == 200
+	 fromId = TRUE
+	 protIds = []
 	 o[seq] = "#{dir}/gi-#{seq.to_s}.fa"
 	 fo = File.open(o[seq], "w")
+	 responseLink.to_str.each_line.grep(/<Id>/) do |ln|
+	    idMatch = /<Id>(\d+)<\/Id>/.match(ln);
+	    unless idMatch.nil?
+	       protIds.push(idMatch[1]) unless fromId
+	       fromId = FALSE
+	    end
+	 end
+	 response = RestClient.post 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi', :db=>'nuccore', :rettype=>'fasta', :id=>protIds.join(',')
+	 abort "Unable to reach NCBI EUtils, error code #{response.code}." unless response.code == 200
 	 fo.puts response.to_str
 	 fo.close
       end
       $stderr.puts "  Reading FastA file: #{o[seq]}" unless o[:q]
-      buffer = ""
-      frgs = 0
       seqs = 0
-      disc = 0
       fi = File.open(o[seq], "r")
       fo = File.open("#{dir}/#{seq.to_s}.fa", "w")
       fi.each_line do |ln|
 	 if /^>(\S+)/.match(ln).nil?
-	    ln.gsub!(/[^A-Za-z]/, '')
-	    buffer = buffer + ln
-	    while buffer.size > o[:win]
-	       frgs += 1
-	       fo.puts ">#{frgs}"
-	       fo.puts buffer[0, o[:win]]
-	       buffer = buffer[o[:step] .. -1]
-	    end
+	    fo.puts ln
 	 else
 	    seqs += 1
-	    disc += buffer.size
-	    buffer = ""
+	    fo.puts ">#{seqs}"
 	 end
       end
       fi.close
       fo.close
-      $stderr.puts "    Created #{frgs} fragments from #{seqs} sequences, discarded #{disc} bp." unless o[:q]
+      $stderr.puts "    File contains #{seqs} sequences." unless o[:q]
       case o[:program].downcase
       when "blast"
-         `"#{o[:bin]}formatdb" -i "#{dir}/#{seq.to_s}.fa" -p F`
+         `"#{o[:bin]}formatdb" -i "#{dir}/#{seq.to_s}.fa" -p T`
       when "blast+"
-         `"#{o[:bin]}makeblastdb" -in "#{dir}/#{seq.to_s}.fa" -dbtype nucl`
+         `"#{o[:bin]}makeblastdb" -in "#{dir}/#{seq.to_s}.fa" -dbtype prot`
       when "blat"
-	 # Nothing to do
+      	 # Nothing to do
       else
          abort "Unsupported program: #{o[:program]}."
       end
@@ -129,16 +128,16 @@ Dir.mktmpdir do |dir|
       s = "#{dir}/seq#{i==1?2:1}.fa"
       case o[:program].downcase
       when "blast"
-	 `"#{o[:bin]}blastall" -p blastn -d "#{s}" -i "#{q}" \
-	 -F F -v 1 -b 1 -a #{o[:thr]} -m 8 -o "#{dir}/#{i}.tab"`
+	 `"#{o[:bin]}blastall" -p blastp -d "#{s}" -i "#{q}" \
+	 -v 1 -b 1 -a #{o[:thr]} -m 8 -o "#{dir}/#{i}.tab"`
 	 #-F F -e 0.001 -v 1 -b 1 -X 150 -a #{o[:thr]} -m 8 -o "#{dir}/#{i}.tab"`
       when "blast+"
-	 `"#{o[:bin]}blastn" -db "#{s}" -query "#{q}" \
-	 -dust no -max_target_seqs 1 \
+	 `"#{o[:bin]}blastp" -db "#{s}" -query "#{q}" \
+	 -max_target_seqs 1 \
 	 -num_threads #{o[:thr]} -outfmt 6 -out "#{dir}/#{i}.tab"`
 	 #-dust no -max_target_seqs 1 -xdrop_ungap 150 -xdrop_gap 150 \
       when "blat"
-	 `#{o[:bin]}blat "#{s}" "#{q}" -out=blast8 "#{dir}/#{i}.tab"`
+	 `#{o[:bin]}blat "#{s}" "#{q}" -prot -out=blast8 "#{dir}/#{i}.tab"`
       else
 	 abort "Unsupported program: #{o[:program]}."
       end
@@ -149,7 +148,7 @@ Dir.mktmpdir do |dir|
       fh.each_line do |ln|
 	 ln.chomp!
 	 row = ln.split(/\t/)
-	 if row[3].to_i >= o[:len] and row[2].to_f >= o[:id]
+	 if row[3].to_i >= o[:len] and row[2].to_f >= o[:id] and row[11].to_f >= o[:bits]
 	    id += row[2].to_f
 	    sq += row[2].to_f ** 2
 	    n  += 1
@@ -167,19 +166,19 @@ Dir.mktmpdir do |dir|
       end
       fh.close
       if n < o[:hits]
-	 puts "Insuffient hits to estimate one-way ANI: #{n}."
-	 res.puts "Insufficient hits to estimate one-way ANI: #{n}"
+	 puts "Insuffient hits to estimate one-way AAI: #{n}."
+	 res.puts "Insufficient hits to estimate one-way AAI: #{n}"
       else
-	 printf "! One-way ANI %d: %.2f%% (SD: %.2f%%), from %i fragments.\n", i, id/n, (sq/n - (id/n)**2)**0.5, n
-	 res.puts sprintf "<b>One-way ANI %d:</b> %.2f%% (SD: %.2f%%), from %i fragments.<br/>", i, id/n, (sq/n - (id/n)**2)**0.5, n unless o[:res].nil?
+	 printf "! One-way AAI %d: %.2f%% (SD: %.2f%%), from %i proteins.\n", i, id/n, (sq/n - (id/n)**2)**0.5, n
+	 res.puts sprintf "<b>One-way AAI %d:</b> %.2f%% (SD: %.2f%%), from %i proteins.<br/>", i, id/n, (sq/n - (id/n)**2)**0.5, n unless o[:res].nil?
       end
    end
    if n2 < o[:hits]
-      puts "Insufficient hits to estimate two-way ANI: #{n2}"
-      res.puts "Insufficient hits to estimate two-way ANI: #{n2}"
+      puts "Insufficient hits to estimate two-way AAI: #{n2}"
+      res.puts "Insufficient hits to estimate two-way AAI: #{n2}"
    else
-      printf "! Two-way ANI  : %.2f%% (SD: %.2f%%), from %i fragments.\n", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2
-      res.puts sprintf "<b>Two-way ANI:</b> %.2f%% (SD: %.2f%%), from %i fragments.<br/>", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2 unless o[:res].nil?
+      printf "! Two-way AAI  : %.2f%% (SD: %.2f%%), from %i proteins.\n", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2
+      res.puts sprintf "<b>Two-way AAI:</b> %.2f%% (SD: %.2f%%), from %i proteins.<br/>", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2 unless o[:res].nil?
    end
    fo.close unless o[:out].nil?
 end
