d9c4b418046a3607ce2b7acf85e76d143953739d
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2014-01-21 16:38:37 -0500

    New scripts
    MyTaxa.fragsBtTax.pl and Table.split.pl were just added.

---
+++ Scripts/MyTaxa.fragsByTax.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+#
+# @author: Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
+# @update: Dec 02 2013
+# @license: artistic license 2.0
+#
+
+use warnings;
+use strict;
+
+my($file,$tax,$rank) = @ARGV;
+($file and $tax) or die "
+.Usage:
+   $0 file.txt taxon[ rank]
+
+   file.txt	MyTaxa output.
+   taxon	Taxon to look for.
+   rank		Rank of taxon (optional). By default: any rank.
+
+";
+$rank ||= ".*";
+$rank = lc $rank;
+
+open MT, "<", $file or die "Cannot read file: $file: $!\n";
+my $last = '';
+while(my $ln=<MT>){
+   chomp $ln;
+   if($ln =~ /<$rank>$tax(;|$)/){
+      $last =~ s/\t.*//;
+      print $last, "\n";
+   }
+   $last = $ln;
+}
+close MT;
+
---
+++ Scripts/Table.split.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+#
+# @author: Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
+# @update: Dec 02 2013
+# @license: artistic license 2.0
+#
+
+use warnings;
+use strict;
+use Getopt::Std;
+use Symbol;
+
+my %o;
+getopts('i:o:e:h', \%o);
+my $file = shift @ARGV;
+
+($file and not $o{h}) or die "
+.Description:
+   Split a file with multiple columns into multiple two-columns lists.
+
+.Usage:
+   $0 [options] file
+   
+   Options:
+      -i <str>	Input field-delimiter.  By default: tabulation (\"\\t\").
+      -o <str>	Prefix of the output files.  By default: no prefix (\"\").
+
+";
+$o{i} ||= "\t";
+$o{o} ||= "";
+
+my $open=0;
+my @fhs=();
+open IN, "<", $file or die "Cannot read file: $file: $!\n";
+while(<IN>){
+   chomp;
+   my @row = split $o{i};
+   my $h = shift @row;
+   if($open){
+      for my $i (0 .. $#row){
+         print { qualify_to_ref $fhs[$i] } $h.$o{i}.$row[$i]."\n" if $row[$i];
+      }
+   }else{
+      $open++;
+      for my $l (@row){
+         $l =~ s/[\.\/:]/_/g;
+	 my $gs = gensym;
+	 open($gs, $o{o}.$l.".txt") or die "Cannot create file: $o{o}$l.txt: $!\n";
+	 push @fhs, $gs;
+      }
+   }
+}
+close IN;
+close $_ for @fhs;
+
