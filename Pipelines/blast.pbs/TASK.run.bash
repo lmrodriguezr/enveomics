2750ec6ee02f07af28ee7c6706deb1af50b3aa9d
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2014-03-23 21:43:06 -0400

    blast.pbs update: Task run verbose and cleaner
    Arrays in step 02 are now cleaned after, removing completed sub-jobs.

--- Pipelines/blast.pbs/TASK.run.bash
+++ Pipelines/blast.pbs/TASK.run.bash
@@ -8,6 +8,7 @@ if [[ "$PDIR" == "" ]] ; then
 fi
 
 # Check if too many auto-trials were attempted
+trials=0 ;
 if [[ -e "$SCRATCH/etc/trials" ]] ; then
    trials=$(cat "$SCRATCH/etc/trials" | wc -l | sed -e 's/ //g');
    if [[ $trials -ge $MAX_TRIALS ]] ; then
@@ -21,6 +22,7 @@ if [[ ! -d "$SCRATCH" ]] ; then mkdir -p "$SCRATCH" || exit 1 ; fi;
 
 if [[ ! -e "$SCRATCH/success/00" ]] ; then
    # 00. Initialize the project
+   echo "00. Initializing project." >&2 ;
    mkdir -p "$SCRATCH/tmp" "$SCRATCH/etc" "$SCRATCH/results" "$SCRATCH/success" || exit 1 ;
    mkdir -p "$SCRATCH/log/active" "$SCRATCH/log/done" "$SCRATCH/log/failed" || exit 1 ;
    mkdir -p "$SCRATCH/log/status" "$SCRATCH/log/eo" || exit 1 ;
@@ -54,15 +56,30 @@ fi ;
 
 if [[ ! -e "$SCRATCH/success/01" ]] ; then
    # 01. Preparing input
+   echo "01. Preparing input." >&2 ;
    JOB01=$(LAUNCH_JOB "01" "00" "Preparing input files" "$SCRATCH/etc/01.bash") ;
+   echo "  New job: $JOB01." >&2 ;
 else
    if [[ ! -e "$SCRATCH/success/02" ]] ; then
       # 02. Launching BLAST
+      echo "02. Launching BLAST." >&2 ;
       JOB02=$(LAUNCH_JOB "02" "00" "Running BLAST" "$SCRATCH/etc/02.bash") ;
+      echo "  New job: $JOB02." >&2 ;
+      if [[ $trials -gt 0 ]] ; then
+	 # Clean on resubmission
+	 echo "  Cleaning completed sub-jobs on $JOB02." >&2 ;
+	 for jobi in $(seq 1 $MAX_JOBS) ; do
+	    if [[ -e "$SCRATCH/success/02.$jobi" ]] ; then
+	       canceljob "$JOB02""[$jobi]" &> /dev/null
+	    fi ;
+	 done ;
+      fi
    else
       if [[ ! -e "$SCRATCH/success/03" ]] ; then
 	 # 03. Finalize
+	 echo "03. Finalizing." >&2 ;
 	 JOB03=$(LAUNCH_JOB "03" "00" "Concatenating results" "$SCRATCH/etc/03.bash") ;
+	 echo "  New job: $JOB03." >&2 ;
       else
          echo "Project complete, nothing to run." ;
       fi ;
