eaffcc8559b6658c0c2c2451065ab1c90f6510de
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2014-04-15 14:01:35 -0400

    New script: FastA.gc.pl

---
+++ Scripts/FastA.gc.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+#
+# @author: Luis M Rodriguez-R
+# @update: Mar 24 2014
+# @license: artistic license 2.0
+#
+
+use warnings;
+use strict;
+
+$#ARGV>=0 or die "
+Usage:
+   $0 seqs.fa... > gc.txt
+
+   seqs.fa	One or more FastA files.
+   gc.txt	A table with the G+C content of the sequences.
+
+";
+
+for my $fa (@ARGV){
+   open FA, "<", $fa or die "Cannot open file: $fa: $!\n";
+   my $def = '';
+   my $len = 0;
+   my $gc  = 0;
+   while(<FA>){
+      if(m/^>(\S+)\s?/){
+         print "$def\t".($gc/$len)."\n" if $def;
+	 $def = $1;
+	 $len = 0;
+	 $gc  = 0;
+      }else{
+         s/[^A-Za-z]//g;
+	 $len += length $_;
+	 s/[^GC]//g;
+	 $gc  += length $_;
+      }
+   }
+   print "$def\t".($gc/$len)."\n" if $len;
+   close FA;
+}
+
