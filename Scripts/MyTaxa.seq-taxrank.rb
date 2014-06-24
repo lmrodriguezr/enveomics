2ed13b38e618f67b1b1ef67105c7de1c70b25ae1
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2014-06-24 17:24:44 -0400

    New script: MyTaxa.seq-taxrank.rb

---
+++ Scripts/MyTaxa.seq-taxrank.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/ruby -w
+
+#
+# @author Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
+# @license artistic license 2.0
+# @update Jun-11-2014
+#
+
+require 'optparse'
+
+opts = {:rank=>'genus', :quiet=>FALSE}
+OptionParser.new do |opt|
+   opt.separator "Generates a simple tabular file with the classification of each sequence at a given taxonomic rank from a MyTaxa output."
+   opt.separator ""
+   opt.on("-i", "--mytaxa FILE", "Input MyTaxa file."){ |v| opts[:mytaxa]=v }
+   opt.on("-r", "--rank STR", "Taxonomic rank.  By default: #{opts[:rank]}."){ |v| opts[:rank] = v.downcase }
+   opt.on("-q","--quiet","Run quietly.") { opts[:quiet]=TRUE }
+   opt.on("-h","--help","Display this screen.") do
+      puts opt
+      exit
+   end
+   opt.separator ""
+end.parse!
+abort "-i/--mytaxa is mandatory." if opts[:mytaxa].nil?
+abort "-i/--mytaxa must exist." unless File.exists? opts[:mytaxa]
+
+begin
+   f = File.open(opts[:mytaxa], "r")
+   ctg = nil;
+   while(ln = f.gets)
+      m = /^(.+)(\t.+){3}/.match(ln)
+      if m
+	 raise "Couldn't find classification for contig #{ctg}" unless ctg.nil?
+	 ctg = m[1]
+      else
+	 raise "Couldn't find the contig name at line #{$.}" if ctg.nil?
+	 m = /<#{opts[:rank]}>([^;]+)/.match(ln)
+	 puts "#{ctg}\t#{m ? m[1] : "Unclassified"}"
+	 ctg = nil
+      end
+   end
+   f.close
+rescue => err
+   $stderr.puts "Exception: #{err}\n\n"
+   err.backtrace.each { |l| $stderr.puts l + "\n" }
+   err
+end
+
