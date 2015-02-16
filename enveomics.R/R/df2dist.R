35ac3caf894e95e7ceabd17cbcd42155fc48ab20
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-02-16 18:27:39 -0500

    enve.df2dist* R functions

--- enveomics.R/R/df2dist.R
+++ enveomics.R/R/df2dist.R
@@ -8,18 +8,21 @@ enve.df2dist <- function(
 	### Index of the column containing the ID of the object 1.
 	obj2.index=2,
 	### Index of the column containing the ID of the object 2.
-	dist.index=3
+	dist.index=3,
 	### Index of the column containing the distance.
+	default.d=NA
+	### Default value (for missing values)
 	){
    x <- as.data.frame(x);
-   x[,obj1.index] <- as.character(x[,obj1.index]);
-   x[,obj2.index] <- as.character(x[,obj2.index]);
-   ids <- unique(c(x[,obj1.index], x[,obj2.index]));
-   m <- matrix(NA, nrow=length(ids), ncol=length(ids), dimnames=list(ids, ids));
-   diag(m) <- 0
+   a <- as.character(x[, obj1.index]);
+   b <- as.character(x[, obj2.index]);
+   d <- as.double(x[, dist.index]);
+   ids <- unique(c(a,b));
+   m <- matrix(default.d, nrow=length(ids), ncol=length(ids), dimnames=list(ids, ids));
+   diag(m) <- 0.0
    for(i in 1:nrow(x)){
-      m[x[i,obj1.index], x[i,obj2.index]] <- x[i,dist.index];
-      m[x[i,obj2.index], x[i,obj1.index]] <- x[i,dist.index];
+      m[a[i], b[i]] <- d[i];
+      m[b[i], a[i]] <- d[i];
    }
    return(as.dist(m));
    ### Returns a `dist` object.
@@ -45,19 +48,19 @@ enve.df2dist.group <- function(
 	### Remove rows with empty or NA groups
 	){
    x <- as.data.frame(x);
-   x[,obj1.index] <- as.character(x[,obj1.index]);
-   x[,obj2.index] <- as.character(x[,obj2.index]);
    if(empty.rm) x <- x[ !(is.na(x[,obj1.index]) | is.na(x[,obj2.index]) | x[,obj1.index]=='' | x[,obj2.index]==''), ]
-   ids <- unique(c(x[,obj1.index], x[,obj2.index]));
+   a <- as.character(x[, obj1.index]);
+   b <- as.character(x[, obj2.index]);
+   d <- as.double(x[, dist.index]);
+   ids <- unique(c(a,b));
+   if(length(ids)<2) return(NA);
    m <- matrix(NA, nrow=length(ids), ncol=length(ids), dimnames=list(ids, ids));
    diag(m) <- 0
    for(i in 2:length(ids)){
       id.i <- ids[i];
       for(j in 1:(i-1)){
 	 id.j <- ids[j];
-	 d.ij <- summary(c(
-	 	x[ x[,obj1.index]==id.i & x[,obj2.index]==id.j, dist.index],
-	 	x[ x[,obj2.index]==id.i & x[,obj1.index]==id.j, dist.index] ));
+	 d.ij <- summary(c( d[ a==id.i & b==id.j], d[ b==id.i & a==id.j] ));
 	 m[id.i, id.j] <- d.ij;
 	 m[id.j, id.i] <- d.ij;
       }
@@ -65,3 +68,45 @@ enve.df2dist.group <- function(
    return(as.dist(m));
    ### Returns a `dist` object.
 }
+
+enve.df2dist.list <- function(
+	### Transform a dataframe (or coercible object, like a table) into a `dist` object.
+	x,
+	### A dataframe (or coercible object) with at least three columns: (1) ID of the object 1,
+	### (2) ID of the object 2, and (3) distance between the two objects.
+	groups,
+	### Named array where the IDs correspond to the object IDs, and the values correspond to
+	### the group.
+	obj1.index=1,
+	### Index of the column containing the ID of the object 1.
+	obj2.index=2,
+	### Index of the column containing the ID of the object 2.
+	dist.index=3,
+	### Index of the column containing the distance.
+	empty.rm=TRUE,
+	### Remove incomplete matrices
+	...
+	### Any other parameters supported by `enve.df2dist.group`.
+	){
+   x <- as.data.frame(x);
+   a <- as.character(x[, obj1.index]);
+   b <- as.character(x[, obj2.index]);
+   d <- as.numeric(x[, dist.index]);
+   ids.all <- unique(c(a,b));
+   l <- list();
+   same_group <- groups[a]==groups[b];
+   same_group <- ifelse(is.na(same_group), FALSE, TRUE);
+   for(group in unique(groups)){
+      ids <- ids.all[ groups[ids.all]==group ];
+      if(length(ids)>1 & group!=""){
+	 x.sub <- x[ same_group & (groups[a]==group) & (groups[b]==group), ]
+	 if(nrow(x.sub)>0){
+	    d.g <- enve.df2dist(x.sub, obj1.index, obj2.index, dist.index, ...);
+	    if(!empty.rm | !any(is.na(d.g))) l[[ group ]] <- d.g;
+	 }
+      }
+   }
+   return(l);
+   ### Returns a `list` of `dist` object.
+}
+
