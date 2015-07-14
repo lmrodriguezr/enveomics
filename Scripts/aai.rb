2493b056235cb75b4662a295918945baeea60083
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-07-13 22:01:22 -0400

    aai.rb new options -a and -S

--- Scripts/aai.rb
+++ Scripts/aai.rb
@@ -2,21 +2,27 @@
 
 #
 # @author: Luis M. Rodriguez-R
-# @update: Jul-05-2015
+# @update: Jul-13-2015
 # @license: artistic license 2.0
 #
 
 require 'optparse'
 require 'tmpdir'
-has_rest_client = TRUE
+has_rest_client = true
+has_sqlite3 = true
 begin
    require 'rubygems'
    require 'restclient'
 rescue LoadError
-   has_rest_client = FALSE
+   has_rest_client = false
+end
+begin
+   require 'sqlite3'
+rescue LoadError
+   has_sqlite3 = false
 end
 
-o = {:len=>0, :id=>20, :bits=>0, :hits=>50, :q=>FALSE, :bin=>'', :program=>'blast+', :thr=>1, :dec=>2}
+o = {:len=>0, :id=>20, :bits=>0, :hits=>50, :q=>FALSE, :bin=>'', :program=>'blast+', :thr=>1, :dec=>2, :auto=>false}
 ARGV << '-h' if ARGV.size==0
 OptionParser.new do |opts|
    opts.banner = "
@@ -45,13 +51,18 @@ Usage: #{$0} [options]"
    opts.on("-t", "--threads INT", "Number of parallel threads to be used.  By default: #{o[:thr]}."){ |v| o[:thr] = v.to_i }
    opts.separator ""
    opts.separator "Other Options"
+   opts.on("-S", "--sqlite3 STR", "Path to the sqlite3 database to create (or update) with the results."){ |v| o[:sqlite3] = v }
+   unless has_sqlite3
+      opts.separator "    Install sqlite3 gem to enable database support."
+   end
    opts.on("-d", "--dec INT", "Decimal positions to report. By default: #{o[:dec]}"){ |v| o[:dec] = v.to_i }
    opts.on("-R", "--rbm FILE", "Saves a file with the reciprocal best matches."){ |v| o[:rbm] = v }
    opts.on("-o", "--out FILE", "Saves a file describing the alignments used for two-way AAI."){ |v| o[:out] = v }
    opts.on("-r", "--res FILE", "Saves a file with the final results."){ |v| o[:res] = v }
    opts.on("-T", "--tab FILE", "Saves a file with the final two-way results in a tab-delimited form.",
       "The columns are (in that order): AAI, standard deviation, proteins used, proteins in the smallest genome."){ |v| o[:tab]=v }
-   opts.on("-q", "--quiet", "Run quietly (no STDERR output)"){ o[:q] = TRUE }
+   opts.on("-q", "--quiet", "Run quietly (no STDERR output)"){ o[:q] = true }
+   opts.on("-a", "--auto", "ONLY outputs the AAI value in STDOUT (or nothing, if calculation fails)."){ o[:auto] = true }
    opts.on("-h", "--help", "Display this screen") do
       puts opts
       exit
@@ -60,6 +71,7 @@ Usage: #{$0} [options]"
 end.parse!
 abort "-1 is mandatory" if o[:seq1].nil?
 abort "-2 is mandatory" if o[:seq2].nil?
+abort "SQLite3 requested (-S) but sqlite3 not supported.  First install gem sqlite3." unless o[:sqlite3].nil? or has_sqlite3
 o[:bin] = o[:bin]+"/" if o[:bin].size > 0
 
 Dir.mktmpdir do |dir|
@@ -123,6 +135,16 @@ Dir.mktmpdir do |dir|
       end
    end
 
+   # Create SQLite3 file
+   seq_names = [File.basename(o[:seq1], ".faa"), File.basename(o[:seq2], ".faa")]
+   unless o[:sqlite3].nil?
+      sqlite_db = SQLite3::Database.new o[:sqlite3]
+      sqlite_db.execute "create table if not exists rbm( seq1 varchar(256), seq2 varchar(256), id1 varchar(256), id2 varchar(256), id float, evalue float, bitscore float )"
+      sqlite_db.execute "create table if not exists aai( seq1 varchar(256), seq2 varchar(256), aai float, sd float, n int, omega int )"
+      sqlite_db.execute "delete from rbm where seq1=? and seq2=?", seq_names
+      sqlite_db.execute "delete from aai where seq1=? and seq2=?", seq_names
+   end
+   
    # Best-hits.
    $stderr.puts "Running one-way comparisons." unless o[:q]
    rbh = []
@@ -176,31 +198,34 @@ Dir.mktmpdir do |dir|
 		  n2  += 1
 		  fo.puts [row[2..5],row[10..11]].join("\t") unless o[:out].nil?
 		  rbm.puts [ori_ids[:seq1][row[1].to_i], ori_ids[:seq2][row[0].to_i], row[2..5], row[8..9], row[6..7], row[10..11]].join("\t") unless o[:rbm].nil?
+		  sqlite_db.execute("insert into rbm values(?,?,?,?,?,?,?)", seq_names + [ori_ids[:seq1][row[1].to_i], ori_ids[:seq2][row[0].to_i], row[2], row[10], row[11]] ) unless o[:sqlite3].nil?
 	       end
 	    end
 	 end
       end
       fh.close
       if n < o[:hits]
-	 puts "Insuffient hits to estimate one-way AAI: #{n}."
+	 puts "Insuffient hits to estimate one-way AAI: #{n}." unless o[:auto]
 	 res.puts "Insufficient hits to estimate one-way AAI: #{n}" unless o[:res].nil?
       else
-	 printf "! One-way AAI %d: %.#{o[:dec]}f%% (SD: %.#{o[:dec]}f%%), from %i proteins.\n", i, id/n, (sq/n - (id/n)**2)**0.5, n
+	 printf "! One-way AAI %d: %.#{o[:dec]}f%% (SD: %.#{o[:dec]}f%%), from %i proteins.\n", i, id/n, (sq/n - (id/n)**2)**0.5, n unless o[:auto]
 	 res.puts sprintf "<b>One-way AAI %d:</b> %.#{o[:dec]}f%% (SD: %.#{o[:dec]}f%%), from %i proteins.<br/>", i, id/n, (sq/n - (id/n)**2)**0.5, n unless o[:res].nil?
       end
    end
    rbm.close unless o[:rbm].nil?
    if n2 < o[:hits]
-      puts "Insufficient hits to estimate two-way AAI: #{n2}"
+      puts "Insufficient hits to estimate two-way AAI: #{n2}" unless o[:auto]
       res.puts "Insufficient hits to estimate two-way AAI: #{n2}" unless o[:res].nil?
    else
-      printf "! Two-way AAI  : %.#{o[:dec]}f%% (SD: %.#{o[:dec]}f%%), from %i proteins.\n", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2
+      printf "! Two-way AAI  : %.#{o[:dec]}f%% (SD: %.#{o[:dec]}f%%), from %i proteins.\n", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2 unless o[:auto]
       res.puts sprintf "<b>Two-way AAI:</b> %.#{o[:dec]}f%% (SD: %.#{o[:dec]}f%%), from %i proteins.<br/>", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2 unless o[:res].nil?
       unless o[:tab].nil?
          tab = File.open(o[:tab], 'w')
 	 tab.printf "%.#{o[:dec]}f\t%.#{o[:dec]}f\t%i\t%i\n", id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2, minfrg
 	 tab.close
       end
+      sqlite_db.execute("insert into aai values(?,?,?,?,?,?)", seq_names + [id2/n2, (sq2/n2 - (id2/n2)**2)**0.5, n2, minfrg]) unless o[:sqlite3].nil?
+      puts id2/n2 if o[:auto]
    end
    res.close unless o[:res].nil?
    fo.close unless o[:out].nil?
