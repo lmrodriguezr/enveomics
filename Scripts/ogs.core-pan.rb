be60b924c4272faa710ce0b295f6d607a6a74698
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-09-01 04:37:18 -0400

    ogs.core-pan.rb: new option --threads

--- Scripts/ogs.core-pan.rb
+++ Scripts/ogs.core-pan.rb
@@ -2,7 +2,7 @@
 
 #
 # @author: Luis M. Rodriguez-R
-# @update: Aug-31-2015
+# @update: Sep-01-2015
 # @license: artistic license 2.0
 #
 
@@ -10,8 +10,9 @@ $:.push File.expand_path(File.dirname(__FILE__) + '/lib')
 require 'enveomics_rb/og'
 require 'optparse'
 require 'json'
+require 'tmpdir'
 
-o = {q:false, a:false, n:10}
+o = {q:false, a:false, n:10, thr:2}
 ARGV << '-h' if ARGV.size==0
 OptionParser.new do |opts|
    opts.banner = "
@@ -30,6 +31,8 @@ Usage: #{$0} [options]"
       ){ |v| o[:n]=v.to_i }
    opts.on("-j", "--json FILE", "Output file in JSON format."){ |v| o[:json]=v }
    opts.on("-t", "--tab FILE","Output file in tabular format."){ |v| o[:tab]=v }
+   opts.on("--threads INT",
+      "Children threads to spawn. By default: #{o[:thr]}."){ |v| o[:thr]=v.to_i}
    opts.on("-s", "--summary FILE",
       "Output file in tabular format with summary statistics."
       ){ |v| o[:summ]=v }
@@ -63,49 +66,71 @@ begin
    
    # Generate subsamples
    size = {core:[], pan:[]}
-   (0 .. o[:n]-1).each do |i|
-      genomes = (0 .. Gene.genomes.length-1).to_a.shuffle
-      size[:core][i] = []
-      size[:pan][i] = []
-      while not genomes.empty?
-	 size[:core][i].unshift(collection.ogs.map do |og|
-	    (genomes - og.genomes).empty? ? 1 : 0
-	 end.inject(0,:+))
-         size[:pan][i].unshift(collection.ogs.map do |og|
-	    ((genomes - og.genomes).length < genomes.length) ? 1 : 0
-	 end.inject(0,:+))
-	 genomes.pop
+   Dir.mktmpdir do |dir|
+      children = 0
+      (0 .. o[:n]-1).each do |i|
+	 fork do
+	    # Generate trajectory
+	    genomes = (0 .. Gene.genomes.length-1).to_a.shuffle
+	    core = []
+	    pan = []
+	    while not genomes.empty?
+	       core.unshift(collection.ogs.map do |og|
+		  (genomes - og.genomes).empty? ? 1 : 0
+	       end.inject(0,:+))
+	       pan.unshift(collection.ogs.map do |og|
+		  ((genomes - og.genomes).length < genomes.length) ? 1 : 0
+	       end.inject(0,:+))
+	       genomes.pop
+	    end
+	    # Store trajectory
+	    tfh = File.open("#{dir}/#{i}", "w")
+	    tfh.puts JSON.generate({core:core, pan:pan})
+	    tfh.close
+	 end # fork
+	 children += 1
+	 if children >= o[:thr]
+	    Process.wait
+	    children -= 1
+	 end
       end
-   end
+      Process.waitall
+      # Recover trajectories
+      (0 .. o[:n]-1).each do |i|
+         s = JSON.parse(File.read("#{dir}/#{i}"), {:symbolize_names=>true})
+	 size[:core][i] = s[:core]
+	 size[:pan][i] = s[:pan]
+      end
+   end # Dir.mktmpdir
 
    # Show result
    $stderr.puts "Generating reports." unless o[:q]
 
    # Save results in JSON
    unless o[:json].nil?
-      ohf = File.open(o[:json], "w")
-      ohf.puts JSON.pretty_generate(size)
-      ohf.close
+      ofh = File.open(o[:json], "w")
+      ofh.puts JSON.pretty_generate(size)
+      ofh.close
    end
 
    # Save results in tab
    unless o[:tab].nil?
-      ohf = File.open(o[:tab], "w")
-      ohf.puts (%w{replicate metric}+(1 .. Gene.genomes.length).to_a).join("\t")
+      ofh = File.open(o[:tab], "w")
+      ofh.puts (%w{replicate metric}+(1 .. Gene.genomes.length).to_a).join("\t")
       (0 .. o[:n]-1).each do |i|
-	 ohf.puts ([i+1,"core"] + size[:core][i]).join("\t")
-	 ohf.puts ([i+1,"pan"] + size[:pan][i]).join("\t")
+	 ofh.puts ([i+1,"core"] + size[:core][i]).join("\t")
+	 ofh.puts ([i+1,"pan"] + size[:pan][i]).join("\t")
       end
-      ohf.close
+      ofh.close
    end
 
    # Save summary results in tab
    unless o[:summ].nil?
-      ohf = File.open(o[:summ], "w")
-      ohf.puts %w{genomes core_avg core_sd core_q1 core_q2 core_q3
+      ofh = File.open(o[:summ], "w")
+      ofh.puts %w{genomes core_avg core_sd core_q1 core_q2 core_q3
 	 pan_avg pan_sd pan_q1 pan_q2 pan_q3}.join("\t")
       (0 .. Gene.genomes.length-1).each do |i|
-	 res = [i+1]
+	 res = [ i+1 ]
 	 [:core, :pan].each do |met|
 	    a = size[met].map{ |r| r[i] }.sort
 	    avg = a.inject(0,:+).to_f / a.size
@@ -115,9 +140,9 @@ begin
 	    q3 = a[ a.size*3/4 ]
 	    res += [avg,sd,q1,q2,q3]
 	 end
-	 ohf.puts res.join("\t")
+	 ofh.puts res.join("\t")
       end
-      ohf.close
+      ofh.close
    end
 
    $stderr.puts "Done.\n" unless o[:q] 
