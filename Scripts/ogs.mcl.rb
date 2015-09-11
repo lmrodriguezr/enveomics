6aa580e3418cf3bfadcc30b2e042c6b27772452f
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-09-11 11:51:42 -0400

    ogs.mcl.rb new option --abc

--- Scripts/ogs.mcl.rb
+++ Scripts/ogs.mcl.rb
@@ -2,17 +2,18 @@
 
 #
 # @author: Luis M. Rodriguez-R
-# @update: Jun-29-2015
+# @update: Sep-11-2015
 # @license: artistic license 2.0
 #
 
-$:.push File.expand_path(File.dirname(__FILE__) + '/lib')
+$:.push File.expand_path(File.dirname(__FILE__) + "/lib")
 require 'enveomics_rb/og'
 require 'optparse'
 require 'tmpdir'
 
-o = {:q=>false, :f=>"(\\S+)-(\\S+)\\.rbm", :mcl=>"", :I=>1.5, :blind=>false, :evalue=>false, :thr=>2, :identity=>false, :bestmatch=>false}
-ARGV << '-h' if ARGV.size==0
+o = {q:false, f:"(\\S+)-(\\S+)\\.rbm", mcl:"", inflation:1.5, blind:false,
+   evalue:false, thr:2, identity:false, bestmatch:false}
+ARGV << "-h" if ARGV.size==0
 OptionParser.new do |opts|
    opts.banner = "
 Identifies Orthology Groups (OGs) in Reciprocal Best Matches (RBM)
@@ -24,18 +25,38 @@ Requires MCL (see http://www.micans.org/mcl).
 Usage: #{$0} [options]"
    opts.separator ""
    opts.separator "Mandatory"
-   opts.on("-o", "--out FILE", "Output file containing the detected OGs."){ |v| o[:out]=v }
-   opts.on("-d", "--dir DIR", "Directory containing the RBM files."){ |v| o[:dir]=v }
+   opts.on("-o", "--out FILE",
+      "Output file containing the detected OGs."){ |v| o[:out]=v }
+   opts.on("-d", "--dir DIR",
+      "Directory containing the RBM files.",
+      "Becomes optional iff --abc is set to a non-empty file."){ |v| o[:dir]=v }
    opts.separator ""
    opts.separator "Other Options"
-   opts.on("-f", "--format STRING", "Format of the filenames for the RBM files (within -d), using regex syntax. By default: '#{o[:f]}'."){ |v| o[:f]=v }
-   opts.on("-I", "--inflation FLOAT", "Inflation parameter for MCL clustering. By default: #{o[:I]}."){ |v| o[:I]=v.to_f }
-   opts.on("-b", "--blind", "If set, computes clusters without taking bitscore into account."){ |v| o[:blind]=v }
-   opts.on("-e", "--evalue", "If set, uses the e-value to weight edges, instead of the default Bit-Score."){ |v| o[:evalue]=v }
-   opts.on("-i", "--identity", "If set, uses the identity to weight edges, instead of the default Bit-Score."){ |v| o[:identity]=v }
-   opts.on("-B", "--best-match", "If set, it assumes best-matches instead reciprocal best matches."){ |v| o[:bestmatch]=v }
-   opts.on("-m", "--mcl-bin DIR", "Path to the directory containing the mcl binaries. By default, assumed to be in the PATH."){ |v| o[:mcl]=v+"/" }
-   opts.on("-t", "--threads INT", "Number of threads to use. By default: #{o[:thr]}."){ |v| o[:thr]=v.to_i }
+   opts.on("-f", "--format STRING",
+      "Format of the filenames for the RBM files (within -d), using regex " +
+      "syntax.", "By default: '#{o[:f]}'."){ |v| o[:f]=v }
+   opts.on("-I", "--inflation FLOAT",
+      "Inflation parameter for MCL clustering. By default: #{o[:inflation]}."
+      ){ |v| o[:inflation]=v.to_f }
+   opts.on("-b", "--blind",
+      "If set, computes clusters without taking bitscore into account."
+      ){ |v| o[:blind]=v }
+   opts.on("-e", "--evalue",
+      "If set, uses the e-value to weight edges, instead of the default " +
+      "Bit-Score."){ |v| o[:evalue]=v }
+   opts.on("-i", "--identity",
+      "If set, uses the identity to weight edges, instead of the default " +
+      "Bit-Score."){ |v| o[:identity]=v }
+   opts.on("-B", "--best-match",
+      "If set, it assumes best-matches instead reciprocal best matches."
+      ){ |v| o[:bestmatch]=v }
+   opts.on("-m", "--mcl-bin DIR",
+      "Path to the directory containing the mcl binaries.",
+      "By default, assumed to be in the PATH."){ |v| o[:mcl]=v+"/" }
+   opts.on("--abc FILE",
+      "Use this abc file instead of a temporal file."){ |v| o[:abc] = v }
+   opts.on("-t", "--threads INT",
+      "Number of threads to use. By default: #{o[:thr]}."){ |v| o[:thr]=v.to_i }
    opts.on("-q", "--quiet", "Run quietly (no STDERR output)."){ o[:q] = true }
    opts.on("-h", "--help", "Display this screen.") do
       puts opts
@@ -44,7 +65,6 @@ Usage: #{$0} [options]"
    opts.separator ""
 end.parse!
 abort "-o is mandatory" if o[:out].nil?
-abort "-d is mandatory" if o[:dir].nil?
 o[:evalue] = false if o[:identity]
 o[:evalue] = false if o[:blind]
 o[:identity] = false if o[:blind]
@@ -52,40 +72,54 @@ o[:identity] = false if o[:blind]
 ##### MAIN:
 begin
    Dir.mktmpdir do |dir|
-      # Read the RBM files in the directory (if -d is passed)
-      abort "-d must exist and be a directory" unless File.exists?(o[:dir]) and File.directory?(o[:dir])
+      o[:abc] = "#{dir}/rbms.abc" if o[:abc].nil?
+      abort "-d must exist and be a directory" unless
+	 File.size?(o[:abc]) or
+	 (!o[:dir].nil? and File.exists?(o[:dir]) and File.directory?(o[:dir]))
       # Traverse the whole directory
-      file_i = 0
-      ln_i = 0
-      $stderr.puts "Reading RBM files within '#{o[:dir]}'." unless o[:q] 
-      abc = File.open("#{dir}/rbms.abc", "w")
-      Dir.entries(o[:dir]).each do |rbm_file|
-	 next unless File.file?(o[:dir]+"/"+rbm_file)
-	 # Parse the filename to identify the genomes
-	 m = /#{o[:f]}/.match(rbm_file)
-	 if m.nil? or m[2].nil?
-	    warn "Cannot parse filename: #{rbm_file} (doesn't match /#{o[:f]}/)."
-	    next
+      if File.size? o[:abc]
+	 $stderr.puts "Reusing existing abc file '#{o[:abc]}'." unless o[:q]
+      else
+	 file_i = 0
+	 ln_i = 0
+	 $stderr.puts "Reading RBM files within '#{o[:dir]}'." unless o[:q]
+	 abc = File.open("#{dir}/rbms.abc", "w")
+	 Dir.entries(o[:dir]).each do |rbm_file|
+	    next unless File.file?(o[:dir]+"/"+rbm_file)
+	    # Parse the filename to identify the genomes
+	    m = /#{o[:f]}/.match(rbm_file)
+	    if m.nil? or m[2].nil?
+	       warn "Ignoring #{rbm_file}: doesn't match /#{o[:f]}/."
+	       next
+	    end
+	    file_i += 1
+	    # Read the RBMs list
+	    f = File.open(o[:dir]+"/"+rbm_file, "r")
+	    while ln = f.gets
+	       # Add the RBM to the abc file
+	       row = ln.split(/\t/)
+	       abc.puts [m[1]+">"+row[0], m[2]+">"+row[1],
+		  (o[:blind] ? "1" :
+		  (o[:evalue] ? row[10] :
+		  (o[:identity] ? row[2] : row[11])))].join("\t")
+	       ln_i += 1
+	    end
+	    f.close
+	    $stderr.print " Scanned files: #{file_i}. " +
+	       "Found RBMs: #{ln_i}.   \r" unless o[:q]
 	 end
-	 file_i += 1
-	 # Read the RBMs list
-	 f = File.open(o[:dir]+"/"+rbm_file, "r")
-	 while ln = f.gets
-	    # Add the RBM to the abc file
-	    row = ln.split(/\t/)
-	    abc.puts [m[1]+">"+row[0], m[2]+">"+row[1], (o[:blind] ? "1" : (o[:evalue] ? row[10] : (o[:identity] ? row[2] : row[11])))].join("\t")
-	    ln_i += 1
-	 end
-	 f.close
-	 $stderr.print " Scanned files: #{file_i}. Found RBMs: #{ln_i}.   \r" unless o[:q]
-      end
-      abc.close
-      $stderr.print "\n" unless o[:q]
+	 abc.close
+	 $stderr.print "\n" unless o[:q]
+      end # if File.size? o[:abc] ... else
 
       # Build .mci file (mcxload) and compute .mccl file (mcl)
       $stderr.puts "Markov-Clustering" unless o[:q]
-      `'#{o[:mcl]}mcxload' #{o[:bestmatch] ? "" : "--stream-mirror"} -abc '#{dir}/rbms.abc' -o '#{dir}/rbms.mci' --write-binary -write-tab '#{dir}/genes.tab' #{o[:evalue] ? "--stream-neg-log10" : ""} &>/dev/null`
-      `'#{o[:mcl]}mcl' '#{dir}/rbms.mci' -V all -I #{o[:I].to_s} -o '#{dir}/ogs.mcl' -te #{o[:thr].to_s}`
+      `'#{o[:mcl]}mcxload' #{"--stream-mirror" unless o[:bestmatch]} \
+	 -abc '#{o[:abc]}' -o '#{dir}/rbms.mci' --write-binary \
+	 -write-tab '#{dir}/genes.tab' #{"--stream-neg-log10" if o[:evalue]} \
+	 &>/dev/null`
+      `'#{o[:mcl]}mcl' '#{dir}/rbms.mci' -V all -I #{o[:inflation].to_s} \
+	 -o '#{dir}/ogs.mcl' -te #{o[:thr].to_s}`
 
       # Load .tab as Gene objects
       $stderr.puts "Loading gene table from '#{dir}/genes.tab'." unless o[:q]
@@ -97,7 +131,8 @@ begin
 	 genes[ r[0].to_i ] = Gene.new(r[1], r[2])
       end
       tab.close
-      $stderr.puts " Got #{genes.size} genes in #{Gene.genomes.size} genomes." unless o[:q]
+      $stderr.puts " Got " + genes.size.to_s + " genes in " +
+	 Gene.genomes.size.to_s + " genomes." unless o[:q]
       
       # Load .mcl file as OGCollection
       $stderr.puts "Loading clusters from '#{dir}/ogs.mcl'." unless o[:q]
@@ -115,11 +150,12 @@ begin
 	 if in_matrix
 	    break if ln =~ /^\)$/
 	    if ln =~ /^\d+\s+/
-	       ln.sub! /^\d+\s+/, ''
+	       ln.sub!(/^\d+\s+/, "")
 	       my_genes = []
 	    end
-	    ln.sub! /^\s+/, ''
-	    raise "Incomplete mcl matrix, offending line: #{$.}: #{ln}" if my_genes.nil?
+	    ln.sub!(/^\s+/, "")
+	    raise "Incomplete mcl matrix, offending line: #{$.}: #{ln}" if
+	       my_genes.nil?
 	    my_genes += ln.split(/\s/)
 	    if my_genes.last == "$"
 	       my_genes.pop
