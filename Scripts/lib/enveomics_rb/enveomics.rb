7cad1ffbdbccc51998f7cd1f34c5512673375173
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-09-29 15:04:18 -0400

    New library: enveomics_rb/enveomics
    Towards more centralized common tasks

---
+++ Scripts/lib/enveomics_rb/enveomics.rb
@@ -0,0 +1,26 @@
+
+#
+# @author: Luis M. Rodriguez-R
+# @update: Sep-29-2015
+# @license: artistic license 2.0
+#
+
+require 'optparse'
+ARGV << "-h" if ARGV.size==0
+
+def use(gems, mandatory=true)
+   gems = [gems] unless gems.is_a? Array
+   begin
+      require 'rubygems'
+      while ! gems.empty?
+         require gems.first
+	 gems.shift
+      end
+      return true
+   rescue LoadError
+      abort "\nUnmet requirements, please install required gems:" +
+	 gems.map{ |gem| "\n   gem install #{gem}" }.join + "\n\n" if mandatory
+      return false
+   end
+end
+
