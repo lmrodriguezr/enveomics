ceb55ad7f5f6496b8ed37482f3eb215f724c5250
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-10-02 16:19:59 -0400

    RefSeq.download.bash major refurbish
    No-wget, additional “level” option, and no mirroring (only species name
    as out-dir)

--- Scripts/RefSeq.download.bash
+++ Scripts/RefSeq.download.bash
@@ -1,31 +1,45 @@
 #!/bin/bash
 
 #
-# @author: Luis M. Rodriguez-R
-# @update: Apr-29-2015
-# @license: artistic license 2.0
+# @author  Luis M. Rodriguez-R
+# @update  Oct-02-2015
+# @license artistic license 2.0
 #
 
 FTP="ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria"
 ORG=$1
 EXT=${2:-.*.gz}
+STT=${3:-Any}
 
 if [[ "$ORG" == "" ]] ; then
-   echo "
-Downloads a collection of assemblies (or annotations) from NCBI's RefSeq.
+echo "
+Downloads a collection of sequences and/or annotations from NCBI's RefSeq.
 
 Usage:
-$0 <organism> [<extension>]
+$0 <organism> [<extension>[ <level>]]
 
 <organism>	The organism to download (e.g., Streptococcus_pneumoniae).
-<extension>	Extension to download. By default: '.*.gz'.
+<extension>	Extension to download.  Common extensions include '.fna.gz'
+		(genomic sequences), '.faa.gz' (protein sequences), and
+		'.gff.gz' (annotations).  By default: '.*.gz' (all data).
+<level>		Use only genomes with this assembly level. Common levels are
+		'Complete Genome' and 'Contig'.  By default, any assembly
+		level is allowed ('Any').
 " >&2
-else
-   wget -m "$FTP/$ORG/assembly_summary.txt"
-   for i in $(curl -s "$FTP/$ORG/latest_assembly_versions/" | awk '{print $9}') ; do
-      wget -m "$FTP/$ORG/latest_assembly_versions/$i/*$EXT"
-   done
+exit
 fi
 
+[[ -d "$ORG" ]] || mkdir "$ORG"
 
+curl "$FTP/$ORG/assembly_summary.txt" -o "$ORG/assembly_summary.txt"
+for path in $(cat "$ORG/assembly_summary.txt" \
+      | awk -F"\t" "\$12==\"$STT\" || \"$STT\"==\"Any\" {print \$20}" ) ; do
+   dir="$ORG/$(basename "$path")"
+   [[ -d "$dir" ]] || mkdir "$dir"
+   for file in $(curl -s "$path/" | awk '{print $9}') ; do
+      if [[ "$file" == $EXT ]] ; then
+	 curl "$path/$file" -o "$dir/$file"
+      fi
+   done
+done
 
