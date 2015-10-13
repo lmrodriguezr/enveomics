95b73262348d6fba6a282fbdb5204fe598022e79
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-10-13 16:53:44 -0400

    Style update

--- Scripts/FastA.slider.pl
+++ Scripts/FastA.slider.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 #
-# @author Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
-# @update Mar-23-2015
+# @author  Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
+# @update  Oct-07-2015
 # @license artistic license 2.0
 # 
 
@@ -30,42 +30,53 @@ Options:
 		default: 0 (discard those chunks).
    -h		Displays this help message and exits.
 
-" if exists $params{'--help'} or exists $params{'-h'} or exists $params{'-help'};
+" if exists $params{'--help'} or
+   exists $params{'-h'} or exists $params{'-help'};
 
-if($params{'-seq'}){ open SEQ, "<", $params{'-seq'} or die "I can not open '".$params{'-seq'}."': $!\n" }
-else{ *SEQ = *STDIN; print STDERR "Please input your sequence, and hit Intro and Ctrl+D when you are done:\n" }
+if($params{'-seq'}){
+   open SEQ, "<", $params{'-seq'} or
+      die "I can not open '".$params{'-seq'}."': $!\n";
+}else{
+   *SEQ = *STDIN;
+   print STDERR "Please input your sequence, and hit ".
+      "Intro and Ctrl+D when you are done:\n";
+}
 
-if($params{'-out'}){ open OUT, ">", $params{'-out'} or die "I can not open '".$params{'-out'}."': $!\n" }
-else{ *OUT = *STDOUT }
+if($params{'-out'}){
+   open OUT, ">", $params{'-out'} or
+      die "I can not open '".$params{'-out'}."': $!\n";
+}else{
+   *OUT = *STDOUT;
+}
 
 $params{'-win'} ||= 18;
 $params{'-step'} ||= 1;
-#$params{'-red'} ||= $params{'-step'}/2;
 $params{'-lerr'} ||= 2;
 $params{'-comm'} ||= 0;
 $params{'-short'} ||= 0;
 
 my $win = $params{'-win'}+0;
 my $stp = $params{'-step'}+0;
-#my $red = $params{'-red'}+0;
 my $lerr = $params{'-lerr'}+0;
 my $buffer = "";
 my $i = 0;
 while(<SEQ>){
-	chomp;
-	if(m/^>/){
-		print OUT ">", ++$i, "\n", $buffer, "\n" if $params{'-short'}==1 and $buffer;
-		$buffer = "";
-		print OUT ";--- INPUT: $_ ---\n" unless $params{'-comm'}==0;
-		next;
-	}
-	s/[^A-Za-z]//g;
-	$buffer.= $_;
-	while(length($buffer) >= $win){
-		print OUT ">", ++$i, "\n", substr($buffer, 0, $win+int(rand($lerr*2)-$lerr)), "\n";
-		#$buffer = substr $buffer, min(0, int(rand($stp*2)-$red));
-		$buffer = substr $buffer, $stp;
-	}
+   next if /^;/;
+   chomp;
+   if(m/^>/){
+      print OUT ">", ++$i, "\n", $buffer, "\n" if
+	 $params{'-short'}==1 and $buffer;
+      $buffer = "";
+      print OUT ";--- INPUT: $_ ---\n" unless $params{'-comm'}==0;
+      next;
+   }
+   s/[^A-Za-z]//g;
+   $buffer.= $_;
+   while(length($buffer) >= $win){
+      print OUT ">", ++$i, "\n",
+	 substr($buffer, 0, $win+int(rand($lerr*2)-$lerr)), "\n";
+      $buffer = substr $buffer, $stp;
+   }
 }
 close SEQ if $params{'-seq'};
 close OUT if $params{'-out'};
