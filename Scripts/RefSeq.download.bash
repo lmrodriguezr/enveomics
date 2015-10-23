31d31d0a41b7727f0e4eebf5b90cfe1938c14712
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-10-23 00:20:59 -0400

    RefSeq.download.bash new option dir

--- Scripts/RefSeq.download.bash
+++ Scripts/RefSeq.download.bash
@@ -2,7 +2,7 @@
 
 #
 # @author  Luis M. Rodriguez-R
-# @update  Oct-02-2015
+# @update  Oct-20-2015
 # @license artistic license 2.0
 #
 
@@ -10,13 +10,14 @@ FTP="ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria"
 ORG=$1
 EXT=${2:-.*.gz}
 STT=${3:-Any}
+DIR=${4:-$ORG}
 
 if [[ "$ORG" == "" ]] ; then
 echo "
 Downloads a collection of sequences and/or annotations from NCBI's RefSeq.
 
 Usage:
-$0 <organism> [<extension>[ <level>]]
+$0 <organism> [<extension>[ <level>[ <dir>]]]
 
 <organism>	The organism to download (e.g., Streptococcus_pneumoniae).
 <extension>	Extension to download.  Common extensions include '.fna.gz'
@@ -25,20 +26,22 @@ $0 <organism> [<extension>[ <level>]]
 <level>		Use only genomes with this assembly level. Common levels are
 		'Complete Genome' and 'Contig'.  By default, any assembly
 		level is allowed ('Any').
+<dir>		Directory where the files are to be downladed. By default,
+		same as <organism>.
 " >&2
 exit
 fi
 
-[[ -d "$ORG" ]] || mkdir "$ORG"
+[[ -d "$DIR" ]] || mkdir "$DIR"
 
-curl "$FTP/$ORG/assembly_summary.txt" -o "$ORG/assembly_summary.txt"
-for path in $(cat "$ORG/assembly_summary.txt" \
+curl -s "$FTP/$ORG/assembly_summary.txt" -o "$DIR/assembly_summary.txt"
+for path in $(cat "$DIR/assembly_summary.txt" \
       | awk -F"\t" "\$12==\"$STT\" || \"$STT\"==\"Any\" {print \$20}" ) ; do
-   dir="$ORG/$(basename "$path")"
+   dir="$DIR/$(basename "$path")"
    [[ -d "$dir" ]] || mkdir "$dir"
    for file in $(curl -s "$path/" | awk '{print $9}') ; do
-      if [[ "$file" == $EXT ]] ; then
-	 curl "$path/$file" -o "$dir/$file"
+      if [[ "$file" == *$EXT ]] ; then
+	 curl -s "$path/$file" -o "$dir/$file"
       fi
    done
 done
