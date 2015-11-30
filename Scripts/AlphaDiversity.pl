623edf9c52c0b7b5e8aa63a1e515f0a9af9e3010
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-11-29 20:16:42 -0500

    AlphaDiversity.pl: New option -C

--- Scripts/AlphaDiversity.pl
+++ Scripts/AlphaDiversity.pl
@@ -2,7 +2,7 @@
 
 #
 # @author: Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
-# @update: Mar-23-2015
+# @update: Nov-29-2015
 # @license: artistic license 2.0
 #
 
@@ -24,23 +24,28 @@ Description:
 Usage:
    $0 [opts]
 
-   -i <str>	* Input table (columns:samples, rows:OTUs, first column:OTU names).
+   -i <str>	* Input table (columns:samples, rows:OTUs, first column:OTU
+		names).
    -r <int>	Number of rows to ignore.  By default: 0.
-   -c <int>	Number of columns to ignore after the first column (i.e., between
-   		the first column, containing the name of the categories, and the
-		first column of abundance values).  By default: 0.
-   -d <str>	Delimiter.  Supported escaped characters are: \"\\t\" (tabulation),
-   		and \"\\0\" (null bit).  By default: \"\\t\".
-   -h		If set, the first row is assumed to have the names of the samples.
-   -D <str>	Distances file.  A squared matrix (or bottom-left half matrix) with
-   		the distances between categories (OTUs or functions).  The first
-		column must contain the names of the categories, and it shouldn't have
-		headers.  If not set, all distances are assumed to be one.  Only used
-		for Rao.
-   -R		Do not calculate Rao indices.  This significantly decreases the total
-   		running time. Note that Rao indices are highly susceptible to precision
-		errors, and shouldn't be trusted for very big numbers.
-   -q <int>	Estimate the qD index (true diversity of order q).  By default: 0.
+   -c <int>	Number of columns to ignore after the first column (i.e.,
+		between the first column, containing the name of the categories,
+		and the first column of abundance values).  By default: 0.
+   -C <int>	Number of columns to ignore at the end of each line.
+		By default: 0.
+   -d <str>	Delimiter.  Supported escaped characters are: \"\\t\"
+		(tabulation), and \"\\0\" (null bit).  By default: \"\\t\".
+   -h		If set, the first row is assumed to have the names of the
+		samples.
+   -D <str>	Distances file.  A squared matrix (or bottom-left half matrix)
+		with the distances between categories (OTUs or functions).  The
+		first column must contain the names of the categories, and it
+		shouldn't have headers.  If not set, all distances are assumed
+		to be one.  Only used for Rao.
+   -R		Do not calculate Rao indices.  This significantly decreases the
+		total running time. Note that Rao indices are highly susceptible
+		to precision errors, and shouldn't be trusted for very big
+		numbers.
+   -q <int>	Estimate the qD index (true diversity order q).  By default: 0.
    --help	This help message.
 
    * Mandatory.
@@ -49,11 +54,12 @@ Usage:
 
 # Input arguments
 my %o;
-getopts('i:c:r:hD:Rq:', \%o);
+getopts('i:c:C:r:hD:Rq:', \%o);
 
 #$o{B} and (eval("use bignum; 1") or die "Cannot use bignum.\n");
 &HELP_MESSAGE() unless $o{i};
 $o{c} ||= 0;
+$o{C} ||= 0;
 $o{r} ||= 0;
 $o{d} ||= "\\t";
 $o{q} ||= 0;
@@ -97,6 +103,7 @@ while(<TABLE>){
    my @ln = split $o{d};
    push @cats, shift(@ln);
    shift @ln for (1 .. $o{c});
+   pop @ln for (1 .. $o{C});
    push @{$values[$_] ||= []}, $ln[$_] for (0 .. $#ln);
    push @{$values[$#ln+1]}, sum(@ln);
 }
@@ -120,9 +127,12 @@ for my $i (0 .. $#values){
 	 my $Qi = 0;
 	 for my $jk (0 .. $#{$values[$i]}){
 	    my $dij = (!$o{D}) ? 1 : 
-		      exists $D->{ $cats[$ik] }->{ $cats[$jk] } ? $D->{ $cats[$ik] }->{ $cats[$jk] } :
-		      exists $D->{ $cats[$jk] }->{ $cats[$ik] } ? $D->{ $cats[$jk] }->{ $cats[$ik] } :
-		      die "Cannot find distance between ".$cats[$ik]." and ".$cats[$jk].".\n";
+	       exists $D->{ $cats[$ik] }->{ $cats[$jk] } ?
+	       $D->{ $cats[$ik] }->{ $cats[$jk] } :
+	       exists $D->{ $cats[$jk] }->{ $cats[$ik] } ?
+	       $D->{ $cats[$jk] }->{ $cats[$ik] } :
+	       die "Cannot find distance between ".$cats[$ik].
+		  " and ".$cats[$jk].".\n";
 	    $Qi += $dij * ($values[$i]->[$ik]/$N) * ($values[$i]->[$jk]/$N);
 	 }
 	 $Q += $Qi;
