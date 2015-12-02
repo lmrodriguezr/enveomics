a007b4a3ef51b165829235b39dbfc7c9696f94ac
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2015-12-01 21:16:45 -0500

    HMMsearch.extractIds.rb update
    Now supporting comments (used by HMMer 3.1b)

--- Scripts/HMMsearch.extractIds.rb
+++ Scripts/HMMsearch.extractIds.rb
@@ -2,27 +2,37 @@
 
 #
 # @author  Luis M. Rodriguez-R <lmrodriguezr at gmail dot com>
-# @update  Feb-06-2015
+# @update  Dec-01-2015
 # @license artistic 2.0
 #
 
-require 'optparse'
+require "optparse"
 
-o = {:quiet=>FALSE, :model=>TRUE}
-ARGV << '-h' if ARGV.size==0
+o = {quiet:false, model:true}
+ARGV << "-h" if ARGV.size==0
 OptionParser.new do |opts|
    opts.banner = "
-Extracts the sequence IDs and query model form a (multiple) HMMsearch report (for HMMer 3.0).
+Extracts the sequence IDs and query model form a (multiple) HMMsearch report
+(for HMMer 3.0).
 
 Usage: #{$0} [options] < input.hmmsearch > list.txt"
    opts.separator ""
    opts.separator "Options"
-   opts.on("-E", "--all-evalue FLOAT", "Maximum e-value of sequence to report result."){|v| o[:all_evalue] = v.to_f }
-   opts.on("-S", "--all-score FLOAT", "Minimum score of sequence to report result."){|v| o[:all_score] = v.to_f }
-   opts.on("-e", "--best-evalue FLOAT", "Maximum e-value of best domain to report result."){|v| o[:best_evalue] = v.to_f }
-   opts.on("-s", "--best-score FLOAT", "Minimum score of best domain to report result."){|v| o[:best_score] = v.to_f }
-   opts.on("-n", "--no-model", "Do not include the query model in the output list."){ o[:model]=FALSE }
-   opts.on("-q", "--quiet", "Run quietly."){ o[:quiet] = TRUE }
+   opts.on("-E", "--all-evalue FLOAT",
+      "Maximum e-value of sequence to report result."
+      ){|v| o[:all_evalue] = v.to_f }
+   opts.on("-S", "--all-score FLOAT",
+      "Minimum score of sequence to report result."
+      ){|v| o[:all_score] = v.to_f }
+   opts.on("-e", "--best-evalue FLOAT",
+      "Maximum e-value of best domain to report result."
+      ){|v| o[:best_evalue] = v.to_f }
+   opts.on("-s", "--best-score FLOAT",
+      "Minimum score of best domain to report result."
+      ){|v| o[:best_score] = v.to_f }
+   opts.on("-n", "--no-model",
+      "Do not include the query model in the output list."){ o[:model]=false }
+   opts.on("-q", "--quiet", "Run quietly."){ o[:quiet]=true }
    opts.on("-h", "--help", "Display this screen.") do
       puts opts
       exit
@@ -47,8 +57,10 @@ ARGF.each_line do |ln|
       end
    when :list
       if /^\s*$/.match(ln).nil?
-         row = ln.split(/\s+/)
-	 raise "Unable to parse seemingly malformed list of hits in line #{$.}:\n#{ln}" unless row.length==11
+         ln.gsub!(/#.*/,"")
+	 row = ln.split(/\s+/)
+	 raise "Unable to parse seemingly malformed list of hits in line " +
+	    "#{$.}:\n#{ln}" unless row.length==11
 	 good = TRUE
 	 good &&= ( o[:all_evalue].nil? || row[1].to_f <= o[:all_evalue] )
 	 good &&= ( o[:all_score].nil? || row[2].to_f >= o[:all_score] )
