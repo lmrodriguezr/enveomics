f4fba74d76f849b592210f16bca41e0294ed55e2
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-01-03 17:11:32 -0500

    New script Newick.autoprune.R + Table.barplot.R renamed
    Also includes example from the new scripts

---
+++ Scripts/Newick.autoprune.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+#
+# @author  Luis M. Rodriguez-R
+# @update  Jan-03-2016
+# @license artistic license 2.0
+#
+
+#= Load stuff
+args <- commandArgs(trailingOnly = F)  
+enveomics_R <- file.path(dirname(
+   sub("^--file=", "", args[grep("^--file=", args)])),
+   "lib", "enveomics.R")
+source(file.path(enveomics_R, "R", "cliopts.R"))
+source(file.path(enveomics_R, "R", "autoprune.R"))
+
+#= Generate interface
+opt <- enve.cliopts(enve.prune.dist,
+   file.path(enveomics_R, "man", "enve.prune.dist.Rd"),
+   positional_arguments=1,
+   usage="usage: %prog [options] output.nwk",
+   mandatory=c("t"),
+   number=c("min_dist","order"),
+   o_desc=list(t="A tree to prune in Newick format."))
+
+#= Run it!
+suppressPackageStartupMessages(library(picante))
+opt$options[['t']] <- read.tree(opt$options[['t']])
+pt <- do.call("enve.prune.dist", opt$options)
+write.tree(pt, opt$args[1])
