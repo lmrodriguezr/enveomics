dc2495beab742044d36ce013da99f857962970e3
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-01-04 16:39:54 -0500

    Bug fixed in Examples/*-matrix.bash

--- Examples/ani-matrix.bash
+++ Examples/ani-matrix.bash
@@ -10,7 +10,7 @@ set -e # <- So it stops if there is an error
 function exists { [[ -e "$1" ]] ; } # <- To test *any* of many files
 
 OUT=$1	# < Output file
-shift
+[[ -n "$1" ]] && shift
 SEQS=$@	# <- list of all genomes
 THR=2	# <- Number or threads
 DEF_DIST=30  # <- Default distance when ANI cannot be reliably estimated (0-100)
@@ -40,7 +40,6 @@ fi
 # 00. Create environment
 export PATH=$(dirname $0)/../Scripts:$PATH
 
-if false ; then
 # 01. Calculate ANI
 echo "[01/03] Calculating ANI"
 for i in $SEQS ; do
@@ -57,8 +56,6 @@ echo "[02/03] Extracting list"
 echo -e "SeqA\tSeqB\tANI\tSD\tN\tOmega\tFrx" > $OUT
 echo "select seq1, seq2, ani, sd, n, omega, (100.0*n/omega) from ani;" \
    | sqlite3 $OUT.db | tr '|' '\t' >> $OUT
-
-fi
 
 # 03. Make it a distance matrix.
 echo "[03/03] Generating distance matrix"
