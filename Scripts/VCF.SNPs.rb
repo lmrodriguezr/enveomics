22e9a84451ae1b2867612448d555c99f72d9e975
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2016-04-06 16:47:44 -0400

    New script: VCF.SNPs.rb

---
+++ Scripts/VCF.SNPs.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+
+# @author  Luis M. Rodriguez-R
+# @update  Apr-06-2016
+# @license Artistic-2.0
+
+$:.push File.expand_path(File.dirname(__FILE__) + "/lib")
+require "enveomics_rb/enveomics"
+require "enveomics_rb/vcf"
+
+o = {min_dp:4, max_dp:Float::INFINITY, min_ref_dp:2, min_alt_dp:2, min_qual:0.0,
+  indels:false, min_ic:0.0}
+OptionParser.new do |opt|
+  opt.banner = "
+  Counts the number of Single-Nucleotide Polymorphisms (SNPs) in a VCF file.
+  
+  Usage: #{$0} [options]".gsub(/^ +/,"")
+  opt.separator ""
+  opt.separator "Mandatory"
+  opt.on("-i", "--input FILE",
+    "Input file in Variant Call Format (VCF)."){ |v| o[:file] = v}
+  opt.separator ""
+  opt.separator "Parameters"
+  opt.on("-m", "--min-dp INT",
+    "Minimum number of reads covering the position. By default: #{o[:min_dp]}."
+    ){ |v| o[:min_dp] = v.to_i }
+  opt.on("-M", "--max-dp INT",
+    "Maximum number of reads covering the position. By default: #{o[:max_dp]}."
+    ){ |v| o[:max_dp] = (v=="Infinity" ? Float::INFINITY : v.to_i) }
+  opt.on("-r", "--min-ref-dp INT",
+    "Minimum number of reads supporting allele REF. " +
+    "By default: #{o[:min_ref_dp]}."
+    ){ |v| o[:min_ref_dp] = v.to_i }
+  opt.on("-a", "--min-alt-dp INT",
+    "Minimum number of reads supporting allele ALT. " +
+    "By default: #{o[:min_alt_dp]}."
+    ){ |v| o[:min_alt_dp] = v.to_i }
+  opt.on("-q", "--min-quality FLOAT",
+    "Minimum quality of the position mapping. By default: #{o[:min_qual]}."
+    ){ |v| o[:max_dp] = v.to_f }
+  opt.on("-s", "--min-shannon FLOAT",
+    "Minimum information content (in bits, from 0 to 1). " +
+    "By default: #{o[:min_ic]}"){ |v| o[:min_ic] = v.to_f }
+  opt.on("--[no-]indels",
+    "Process (or ignore) indels. By default: ignore."
+    ){ |v| o[:indels] = v }
+  opt.on("-h", "--help", "Display this screen.") do
+    puts opt
+    exit
+  end
+  opt.separator ""
+end.parse!
+
+abort "--input is mandatory" if o[:file].nil?
+
+vcf = VCF.new(o[:file])
+c = 0
+dp = 0
+ref_dp = 0
+alt_dp = 0
+h = 0
+vcf.each_variant do |v|
+  next if v.indel? and not o[:indels]
+  next if v.dp < o[:min_dp]
+  next if v.dp > o[:max_dp]
+  next if v.ref_dp < o[:min_ref_dp]
+  next if v.alt_dp < o[:min_alt_dp]
+  next if v.qual < o[:min_qual]
+  next if v.shannon < o[:min_ic]
+  c += 1
+  dp += v.dp
+  ref_dp += v.ref_dp
+  alt_dp += v.alt_dp
+  h += v.shannon
+end
+puts "SNPs: #{c}", "Information content: #{h}",
+  "Average SNP depth: #{dp.to_f/c}",
+  "Average REF allele depth: #{ref_dp.to_f/c}",
+  "Average ALT allele depth: #{alt_dp.to_f/c}"
+
---
+++ Scripts/lib/enveomics_rb/vcf.rb
@@ -0,0 +1,100 @@
+# @author  Luis M. Rodriguez-R
+# @update  Apr-06-2016
+# @license Artistic-2.0
+
+##
+# Enveomics representation of a Variant Call Format (VCF) file.
+class VCF
+  
+  ##
+  # File-handler, a File object.
+  attr_reader :fh
+  def initialize(file)
+    @fh = (file.is_a?(String) ? File.open(file, "r") : file )
+  end
+  
+  ##
+  # Iterate through each variant (i.e., each non-comment line), passing a
+  # VCF::Variant object to +blk+.
+  def each_variant(&blk)
+    fh.rewind
+    fh.each_line do |ln|
+      next if ln =~ /^#/
+      blk.call VCF::Variant.new(ln)
+    end
+  end
+end
+
+class VCF::Variant
+  
+  ##
+  # Column definitions in VCF.
+  @@COLUMNS = [:chrom,:pos,:id,:ref,:alt,:qual,:filter,:info,:format,:bam]
+  
+  ##
+  # An Array of String, containing each of the VCF entrie's columns.
+  attr_reader :data
+
+  ##
+  # Initialize VCF::Variant from String +line+, a non-comment line in the VCF.
+  def initialize(line)
+    @data = line.chomp.split("\t")
+    # Qual as float
+    @data[5] = data[5].to_f
+    # Split info
+    info = data[7].split(";").map{ |i| i=~/=/ ? i.split("=", 2) : [i, true] }
+    @data[7] = Hash[*info.map{ |i| [i[0].to_sym, i[1]] }.flatten]
+  end
+
+  ##
+  # Named functions for each column.
+  @@COLUMNS.each_index do |i|
+    define_method(@@COLUMNS[i]) { data[i] }
+  end
+
+  ##
+  # Sequencing depth.
+  def dp
+    return nil if info[:DP].nil?
+    info[:DP].to_i
+  end
+
+  ##
+  # Sequencing depth of FWD-REF, REV-REF, FWD-ALT, and REV-ALT.
+  def dp4
+    return nil if info[:DP4].nil?
+    @dp4 ||= info[:DP4].split(",").map{ |i| i.to_i }
+    @dp4
+  end
+  
+  ##
+  # Sequencing depth of the REF allele.
+  def ref_dp
+    dp = dp4
+    dp.nil? ? nil : dp[0] + dp[1]
+  end
+
+  ##
+  # Sequencing depth of the ALT allele.
+  def alt_dp
+    dp = dp4
+    dp.nil? ? nil : dp[2] + dp[3]
+  end
+
+  ##
+  # Information content of the variant in bits (from 0 to 1).
+  def shannon
+    return @shannon unless @shannon.nil?
+    a = ref_dp
+    b = alt_dp
+    ap = a.to_f/(a+b)
+    bp = b.to_f/(a+b)
+    @shannon = -(ap*Math.log(ap,2) + bp*Math.log(bp,2))
+    @shannon
+  end
+
+  ##
+  # Is it an indel?
+  def indel? ; !info[:INDEL].nil? and info[:INDEL] ; end
+  
+end
