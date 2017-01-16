85ee801f343e9fcce37bfa8d38efdf3e861dca12
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2017-01-16 15:47:42 -0500

    New script NCBIacc2tax.rb
    Related to #22

--- Scripts/NCBIacc2tax.rb
+++ Scripts/NCBIacc2tax.rb
@@ -11,29 +11,26 @@ use "nokogiri"
 
 #================================[ Options parsing ]
 o = {
-  :q=>false, :gis=>[], :dbfrom=>"nuccore", :header=>true,
-  :exact_gi=>false, :no_nil=>false, :ret=>"ScientificName",
+  :q=>false, :accs=>[], :dbfrom=>"nuccore", :header=>true,
+  :no_nil=>false, :ret=>"ScientificName",
   :ranks=>%w(superkingdom phylum class order family genus species)}
 OptionParser.new do |opt|
   opt.banner = "
-  Maps a list of NCBI GIs to their corresponding taxonomy using the NCBI
+  Maps a list of NCBI accessions to their corresponding taxonomy using the NCBI
   EUtilities. Avoid using this script on millions of entries at a time, since
   each entry elicits two requests to NCBI's servers.
 
-  *IMPORTANT NOTE*: NCBI is phasing out support for GIs. Please use acc.ver
-  instead with NCBIacc2tax.rb.
-
   Usage: #{$0} [options]".gsub(/^ +/,"")
   opt.separator ""
-  opt.on("-g", "--gis GI1,GI2,...", Array,
-    "Comma-separated list of GIs. Required unless -i is passed."
-    ){ |v| o[:gis]=v }
+  opt.on("-a", "--acc acc1,acc2.ver,...", Array,
+    "Comma-separated list of accessions. Required unless -i is passed."
+    ){ |v| o[:accs]=v }
   opt.on("-i", "--infile FILE",
-    "Raw text file containing the list of GIs, one per line.",
+    "Raw text file containing the list of accessions, one per line.",
     "Required unless -g is passed."){ |v| o[:infile]=v }
   opt.on("-p", "--protein",
-    "Use if the GIs are proteins. Otherwise, GIs are assumed to be from " +
-    "the Nuccore Database."){ o[:dbfrom]="protein" }
+    "Use if the accessions are proteins. Otherwise, accessions are assumed " +
+    "to be from the Nuccore Database."){ o[:dbfrom]="protein" }
   opt.on("-r", "--ranks RANK1,RANK2,...", Array,
     "Taxonomic ranks to report. By default: #{o[:ranks].join(",")}."
     ){ |v| o[:ranks]=v }
@@ -41,13 +38,9 @@ OptionParser.new do |opt|
     "Do not include a header in the output."){ o[:header]=false }
   opt.on("-t", "--taxids",
     "Return Taxonomy IDs instead of scientific names."){ o[:ret]="TaxId" }
-  opt.on("--exact-gi",
-    "Returns only taxonomy associated with the exact GI passed.",
-    "By default, it attempts to update accession versions if possible."
-    ){ |v| o[:exact_gi]=v }
   opt.on("--ignore-missing",
-    "Does not report missing GIs in the output file.",
-    "By default, it reports GI and empty values for all other columns."
+    "Does not report missing accessions in the output file.",
+    "By default, it reports accessions and empty values for all other columns."
     ){ |v| o[:no_nil]=v }
   opt.on("-q", "--quiet", "Run quietly."){ |v| o[:q]=true }
   opt.on("-h", "--help","Display this screen") do
@@ -58,28 +51,24 @@ OptionParser.new do |opt|
 end.parse!
 
 #================================[ Functions ]
-def gi2taxid(db, gi)
+def acc2taxid(db, acc)
   doc = Nokogiri::XML( RemoteData.elink({:dbfrom=>db,
-    :db=>"taxonomy", :id=>gi}) )
+    :db=>"taxonomy", :id=>acc, :idtype=>"acc"}) )
   doc.at_xpath("/eLinkResult/LinkSet/LinkSetDb/Link/Id")
 end
 #================================[ Main ]
 begin
-  o[:gis] += File.readlines(o[:infile]).map{ |l| l.chomp } unless
+  o[:accs] += File.readlines(o[:infile]).map{ |l| l.chomp } unless
     o[:infile].nil?
   o[:ranks].map!{ |r| r.downcase }
-  puts (["GI", "TaxId"] + o[:ranks].map{ |r| r.capitalize }).join("\t") if
+  puts (["Acc", "TaxId"] + o[:ranks].map{ |r| r.capitalize }).join("\t") if
     o[:header]
-  o[:gis].each do |gi|
-    taxid = gi2taxid(o[:dbfrom], gi)
+  o[:accs].each do |acc|
+    taxid = acc2taxid(o[:dbfrom], acc)
     status = ""
-    if taxid.nil? and not o[:exact_gi]
-      new_gi, status = RemoteData.update_gi(o[:dbfrom], gi)
-      taxid = gi2taxid(o[:dbfrom], new_gi) unless new_gi.nil?
-    end
     if taxid.nil?
-      warn "Cannot find link to taxonomy: #{gi} #{status}"
-      puts ([gi, ""] + o[:ranks].map{ |i| "" }).join("\t") unless o[:no_nil]
+      warn "Cannot find link to taxonomy: #{acc} #{status}"
+      puts ([acc, ""] + o[:ranks].map{ |i| "" }).join("\t") unless o[:no_nil]
       next
     end
     taxonomy = {}
@@ -93,7 +82,7 @@ begin
           taxon.at_xpath("./#{o[:ret]}").content
       end
     end
-    puts ([gi, taxid.content] +
+    puts ([acc, taxid.content] +
       o[:ranks].map{ |rank| taxonomy[ rank ] ||= "" }).join("\t")
   end
 rescue => err
--- Scripts/lib/enveomics_rb/remote_data.rb
+++ Scripts/lib/enveomics_rb/remote_data.rb
@@ -1,7 +1,6 @@
 
 #
 # @author: Luis M. Rodriguez-R
-# @update: Oct-13-2015
 # @license: artistic license 2.0
 #
 
@@ -10,66 +9,66 @@ use "restclient"
 use "json"
 
 class RemoteData
-   # Class-level variables
-   @@EUTILS = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
-   @@EBIREST = "http://www.ebi.ac.uk/Tools"
+  # Class-level variables
+  @@EUTILS = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
+  @@EBIREST = "http://www.ebi.ac.uk/Tools"
    
-   # Class-level methods
-   def self.eutils(script, params={}, outfile=nil)
-      response = nil
-      10.times do
-	 begin
-	    response = RestClient.get "#{@@EUTILS}/#{script}", {:params=>params}
-	 rescue => err
-	    warn "Request failed #{response.nil? ? "without error code" :
-	       "with error code #{response.code}"}."
-	    next
-	 end
-	 break if response.code == 200
+  # Class-level methods
+  def self.eutils(script, params={}, outfile=nil)
+    response = nil
+    10.times do
+      begin
+        response = RestClient.get "#{@@EUTILS}/#{script}", {:params=>params}
+      rescue => err
+        warn "Request failed #{response.nil? ? "without error code" :
+          "with error code #{response.code}"}."
+        next
       end
-      abort "Unable to reach NCBI EUtils, error code #{response.code}." unless
-	 response.code == 200
-      unless outfile.nil?
-	 ohf = File.open(outfile, "w")
-	 ohf.print response.to_s
-	 ohf.close
-      end
-      response.to_s
-   end
-   def self.efetch(*etc)
-      eutils "efetch.fcgi", *etc
-   end
-   def self.elink(*etc)
-      eutils "elink.fcgi", *etc
-   end
-   def self.esummary(*etc)
-      eutils "esummary.fcgi", *etc
-   end
-   def self.update_gi(db, old_gi)
-      summ = JSON.parse RemoteData.esummary({:db=>db, :id=>old_gi,
-	 :retmode=>"json"})
-      return nil,nil if summ["result"].nil? or summ["result"][old_gi.to_s].nil?
-      new_acc = summ["result"][old_gi.to_s]["replacedby"]
-      new_gi = (new_acc.nil? ? nil :
-	 RemoteData.efetch({:db=>db, :id=>new_acc, :rettype=>"gi"}))
-      return new_gi,summ["result"][old_gi.to_s]["status"]
-   end
-   def self.ebiFetch(db, id, format, outfile=nil)
-      url = "#{@@EBIREST}/dbfetch/dbfetch/#{db}/#{id}/#{format}"
-      response = RestClient::Request.execute(:method=>:get,
-	 :url=>url, :timeout=>600)
-      raise "Unable to reach EBI REST client, error code " +
-	 response.code.to_s + "." unless response.code == 200
-      response.to_s
-   end
-   def self.ebiseq2taxid(id,db)
-      doc = RemoteData.ebiFetch(db, id, "annot").split(/[\n\r]/)
-      ln = doc.grep(/^FT\s+\/db_xref="taxon:/).first
-      ln = doc.grep(/^OX\s+NCBI_TaxID=/).first if ln.nil?
-      return nil if ln.nil?
-      ln.sub!(/.*(?:"taxon:|NCBI_TaxID=)(\d+)["; ].*/, "\\1")
-      return nil unless ln =~ /^\d+$/
-      ln
-   end
+      break if response.code == 200
+    end
+    abort "Unable to reach NCBI EUtils, error code #{response.code}." unless
+      response.code == 200
+    unless outfile.nil?
+      ohf = File.open(outfile, "w")
+      ohf.print response.to_s
+      ohf.close
+    end
+    response.to_s
+  end
+  def self.efetch(*etc)
+    eutils "efetch.fcgi", *etc
+  end
+  def self.elink(*etc)
+    eutils "elink.fcgi", *etc
+  end
+  def self.esummary(*etc)
+    eutils "esummary.fcgi", *etc
+  end
+  def self.update_gi(db, old_gi)
+    summ = JSON.parse RemoteData.esummary({:db=>db, :id=>old_gi,
+      :retmode=>"json"})
+    return nil,nil if summ["result"].nil? or summ["result"][old_gi.to_s].nil?
+    new_acc = summ["result"][old_gi.to_s]["replacedby"]
+    new_gi = (new_acc.nil? ? nil :
+      RemoteData.efetch({:db=>db, :id=>new_acc, :rettype=>"gi"}))
+    return new_gi,summ["result"][old_gi.to_s]["status"]
+  end
+  def self.ebiFetch(db, id, format, outfile=nil)
+    url = "#{@@EBIREST}/dbfetch/dbfetch/#{db}/#{id}/#{format}"
+    response = RestClient::Request.execute(:method=>:get,
+      :url=>url, :timeout=>600)
+    raise "Unable to reach EBI REST client, error code " +
+      response.code.to_s + "." unless response.code == 200
+    response.to_s
+  end
+  def self.ebiseq2taxid(id,db)
+    doc = RemoteData.ebiFetch(db, id, "annot").split(/[\n\r]/)
+    ln = doc.grep(/^FT\s+\/db_xref="taxon:/).first
+    ln = doc.grep(/^OX\s+NCBI_TaxID=/).first if ln.nil?
+    return nil if ln.nil?
+    ln.sub!(/.*(?:"taxon:|NCBI_TaxID=)(\d+)["; ].*/, "\\1")
+    return nil unless ln =~ /^\d+$/
+    ln
+  end
 end
 
