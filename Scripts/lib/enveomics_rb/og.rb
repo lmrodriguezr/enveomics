64607ea782301acbaddb6b0f2f874614d0eb3a8f
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2017-05-18 22:44:04 -0400

    ogs.core-pan.rb completely revamped
    The ogs.core-pan.rb has been modified to boost its efficiency, and is
    now several orders of magnitude faster. The new version bypasses the
    use of enveomics_rb/og and uses instead binary operations to measure
    core and pangenomes.

--- Scripts/lib/enveomics_rb/og.rb
+++ Scripts/lib/enveomics_rb/og.rb
@@ -2,169 +2,181 @@
 ##### CLASSES:
 # Gene.new(genome, id): Initializes a new Gene.
 # genome: A string uniquely identifying the parent genome.
-# id: A string uniquely identifying the gene within the genome. It can be non-unique across genomes.
+# id: A string uniquely identifying the gene within the genome. It can be
+#   non-unique across genomes.
 class Gene
-   attr_reader :genome_id, :id
-   @@genomes = []
-   def self.genomes
-      @@genomes
-   end
-   def initialize(genome, id)
-      if genome.is_a? Integer
-         abort "Internal error: Genome #{genome} does not exist yet." if @@genomes[genome].nil?
-	 @genome_id = genome
-      else
-	 @@genomes << genome unless @@genomes.include? genome
-	 @genome_id = @@genomes.index(genome)
-      end
-      @id = id
-   end
-   # Compare if two Gene objects refer to the same gene.
-   def ==(b)
-      self.genome_id==b.genome_id and self.id==b.id
-   end
-   # Get all genomes in the run as an array of strings.
-   def genome
-      @@genomes[self.genome_id]
-   end
-   def to_s
-      "#{self.genome}:#{self.id}"
-   end
+  attr_reader :genome_id, :id
+  @@genomes = []
+  def self.genomes
+    @@genomes
+  end
+  def initialize(genome, id)
+    if genome.is_a? Integer
+      abort "Internal error: Genome #{genome} does not exist yet." if
+        @@genomes[genome].nil?
+      @genome_id = genome
+    else
+      @@genomes << genome unless @@genomes.include? genome
+      @genome_id = @@genomes.index(genome)
+    end
+    @id = id
+  end
+  # Compare if two Gene objects refer to the same gene.
+  def ==(b)
+    self.genome_id==b.genome_id and self.id==b.id
+  end
+  # Get all genomes in the run as an array of strings.
+  def genome
+    @@genomes[self.genome_id]
+  end
+  def to_s
+    "#{self.genome}:#{self.id}"
+  end
 end
 
 # OG.new(): Initializes an empty OG.
 # OG.new(genomes, genes): Initializes a pre-computed OG.
 # genomes: List of genomes as an array of strings (as in Gene.genomes).
-# genes: List of genes as an array of strings, with '-' indicating no genes and multiple genes separated by ','.
+# genes: List of genes as an array of strings, with '-' indicating no genes and
+#   multiple genes separated by ','.
 class OG
-   attr_reader :genes, :notes
-   def initialize(genomes=nil, genes=nil)
-      @genes = []
-      @notes = []
-      unless genomes.nil? or genes.nil?
-	 (0 .. genes.length-1).each do |genome_i|
-	    next if genes[genome_i]=="-"
-	    genes[genome_i].split(/,/).each do |gene_id|
-	       self << Gene.new(genomes[genome_i], gene_id)
-	    end
-	 end
+  attr_reader :genes, :notes
+  def initialize(genomes=nil, genes=nil)
+    @genes = []
+    @notes = []
+    unless genomes.nil? or genes.nil?
+      (0 .. genes.length-1).each do |genome_i|
+        next if genes[genome_i]=="-"
+        genes[genome_i].split(/,/).each do |gene_id|
+          self << Gene.new(genomes[genome_i], gene_id)
+        end
       end
-   end
-   # Add genes or combine another OG into the loaded OG (self).
-   def <<(obj)
-      if obj.is_a? Gene
-	 @genes[obj.genome_id] = [] if @genes[obj.genome_id].nil?
-	 @genes[obj.genome_id] << obj.id unless self.include? obj
-      elsif obj.is_a? OG
-	 obj.genes_obj.each{ |gene| self << gene }
-      else
-	 abort "Unsupported class for #{obj}"
-      end
-   end
-   # Get the list of genes as objects (internally saved as strings to save RAM).
-   def genes_obj
-      o = []
-      (0 .. Gene.genomes.length-1).map do |genome_id|
-         o += self.genes[genome_id].map{ |gene_id| Gene.new(Gene.genomes[genome_id], gene_id) } unless self.genes[genome_id].nil?
-      end
-      return o
-   end
-   # Evaluates if the OG contains the passed gene.
-   def include?(gene)
-      return false if self.genes[gene.genome_id].nil?
-      self.genes[gene.genome_id].include? gene.id
-   end
-   # Get the list of genomes containing genes in this OG.
-   def genomes
-      (0 .. Gene.genomes.length-1).select do |gno|
-         not(self.genes[gno].nil? or self.genes[gno].empty?)
-      end
-   end
-   # Adds a note that will be printed after the last column
-   def add_note note, note_idx=nil
-      if note_idx.nil?
-	 @notes << note
-      else
-	 @notes[note_idx] = (@notes[note_idx].nil? ? '' : (@notes[note_idx]+' || ')) + note
-      end
-   end
-   def to_s
-      (0 .. Gene.genomes.length-1).map do |genome_id|
-	 self.genes[genome_id].nil? ? "-" : self.genes[genome_id].join(",")
-      end.join("\t") + ((self.notes.size==0) ? '' : ("\t#\t"+self.notes.join("\t")))
-   end
+    end
+  end
+  # Add genes or combine another OG into the loaded OG (self).
+  def <<(obj)
+    if obj.is_a? Gene
+      @genes[obj.genome_id] = [] if @genes[obj.genome_id].nil?
+      @genes[obj.genome_id] << obj.id unless self.include? obj
+    elsif obj.is_a? OG
+      obj.genes_obj.each{ |gene| self << gene }
+    else
+      abort "Unsupported class for #{obj}"
+    end
+  end
+  # Get the list of genes as objects (internally saved as strings to save RAM).
+  def genes_obj
+    o = []
+    (0 .. Gene.genomes.length-1).map do |genome_id|
+      o += self.genes[genome_id].map{ |gene_id|
+        Gene.new(Gene.genomes[genome_id], gene_id) } unless
+          self.genes[genome_id].nil?
+    end
+    return o
+  end
+  # Evaluates if the OG contains the passed gene.
+  def include?(gene)
+    return false if self.genes[gene.genome_id].nil?
+    self.genes[gene.genome_id].include? gene.id
+  end
+  # Get the list of genomes containing genes in this OG.
+  def genomes
+    (0 .. Gene.genomes.length-1).select do |gno|
+      not(self.genes[gno].nil? or self.genes[gno].empty?)
+    end
+  end
+  # Adds a note that will be printed after the last column
+  def add_note note, note_idx=nil
+    if note_idx.nil?
+      @notes << note
+    else
+      @notes[note_idx] = (@notes[note_idx].nil? ? '' :
+        (@notes[note_idx]+' || ')) + note
+    end
+  end
+  def to_s
+    (0 .. Gene.genomes.length-1).map do |genome_id|
+      self.genes[genome_id].nil? ? "-" : self.genes[genome_id].join(",")
+    end.join("\t") + ((self.notes.size==0) ? '' :
+      ("\t#\t"+self.notes.join("\t")))
+  end
+  def to_bool_a
+    (0 .. Gene.genomes.length-1).map { |genome_id| not genes[genome_id].nil? }
+  end
 end
 
 # OGCollection.new(): Initializes an empty collection of OGs.
 class OGCollection
-   attr_reader :ogs, :note_srcs
-   def initialize
-      @ogs = []
-      @note_srcs = []
-   end
-   # Add an OG to the collection
-   def <<(og)
+  attr_reader :ogs, :note_srcs
+  def initialize
+    @ogs = []
+    @note_srcs = []
+  end
+  # Add an OG to the collection
+  def <<(og)
+    @ogs << og
+  end
+  # Compare OGs all-vs-all to identify groups that should be merged.
+  def consolidate!
+    old_ogs = self.ogs
+    @ogs = []
+    old_ogs.each do |og|
+      is_new = true
+      og.genes_obj.each do |gene|
+        o = self.get_og gene
+        unless o.nil?
+          o << og
+          is_new = false
+          break
+        end
+      end
+      self << og if is_new
+    end
+  end
+  # Removes OGs present in less than 'fraction' of the genomes
+  def filter_core!(fraction=1.0)
+    min_genomes = (fraction * Gene.genomes.size).ceil
+    @ogs.select! { |og| og.genomes.size >= min_genomes }
+  end
+  # Removes OGs present more than 'dups' number of times in any genome
+  def remove_inparalogs!(dups=1)
+    @ogs.select! do |og|
+      og.genes.map{ |pergenome| pergenome.size }.max <= dups
+    end
+  end
+  # Add a pair of RBM genes into the corresponding OG, or create a new OG.
+  def add_rbm(a, b)
+    og = self.get_og(a)
+    og = self.get_og(b) if og.nil?
+    if og.nil?
+      og = OG.new
       @ogs << og
-   end
-   # Compare OGs all-vs-all to identify groups that should be merged.
-   def consolidate!
-      old_ogs = self.ogs
-      @ogs = []
-      old_ogs.each do |og|
-	 is_new = true
-	 og.genes_obj.each do |gene|
-	    o = self.get_og gene
-	    unless o.nil?
-	       o << og
-	       is_new = false
-	       break
-	    end
-	 end
-	 self << og if is_new
-      end
-   end
-   # Removes OGs present in less than 'fraction' of the genomes
-   def filter_core!(fraction=1.0)
-      min_genomes = (fraction * Gene.genomes.size).ceil
-      @ogs.select! { |og| og.genomes.size >= min_genomes }
-   end
-   # Removes OGs present more than 'dups' number of times in any genome
-   def remove_inparalogs!(dups=1)
-      @ogs.select! do |og|
-	 og.genes.map{ |pergenome| pergenome.size }.max <= dups
-      end
-   end
-   # Add a pair of RBM genes into the corresponding OG, or create a new OG.
-   def add_rbm(a, b)
-      og = self.get_og(a)
-      og = self.get_og(b) if og.nil?
-      if og.nil?
-	 og = OG.new
-	 @ogs << og
-      end
-      og << a
-      og << b
-   end
-   # Get the OG containing the gene (returns the first, if multiple).
-   def get_og(gene)
-      idx = self.ogs.index { |og| og.include? gene }
-      idx.nil? ? nil : self.ogs[idx]
-   end
-   # Get the genes from a given genome (returns an array of arrays)
-   def get_genome_genes(genome)
-      genome_id = Gene.genomes.index(genome)
-      self.ogs.map do |og|
-         g = og.genes[genome_id]
-	 g.nil? ? [] : g
-      end
-   end
-   # Add annotation sources
-   def add_note_src src
-      @note_srcs << src
-   end
-   def to_s
-      Gene.genomes.join("\t") + ((self.note_srcs.length>0) ? ("\t#\t"+self.note_srcs.join("\t")) : '') +
-	 "\n" + self.ogs.map{ |og| og.to_s }.join("\n")
-   end
+    end
+    og << a
+    og << b
+  end
+  # Get the OG containing the gene (returns the first, if multiple).
+  def get_og(gene)
+    idx = self.ogs.index { |og| og.include? gene }
+    idx.nil? ? nil : self.ogs[idx]
+  end
+  # Get the genes from a given genome (returns an array of arrays)
+  def get_genome_genes(genome)
+    genome_id = Gene.genomes.index(genome)
+    self.ogs.map do |og|
+      g = og.genes[genome_id]
+      g.nil? ? [] : g
+    end
+  end
+  # Add annotation sources
+  def add_note_src src
+    @note_srcs << src
+  end
+  def to_s
+    Gene.genomes.join("\t") + ((self.note_srcs.length>0) ?
+        ("\t#\t"+self.note_srcs.join("\t")) : '') +
+      "\n" + self.ogs.map{ |og| og.to_s }.join("\n")
+  end
+  def to_bool_a ; ogs.map{ |og| og.to_bool_a } ; end
 end
 
--- Scripts/ogs.core-pan.rb
+++ Scripts/ogs.core-pan.rb
@@ -2,155 +2,159 @@
 
 #
 # @author: Luis M. Rodriguez-R
-# @license: artistic license 2.0
-# @update: Sep-11-2015
+# @license: artistic-2.0
 #
 
-$:.push File.expand_path(File.dirname(__FILE__) + '/lib')
-require 'enveomics_rb/og'
-require 'optparse'
-require 'json'
-require 'tmpdir'
+$:.push File.expand_path("../lib", __FILE__)
+require "optparse"
+require "json"
+require "tmpdir"
 
-o = {q:false, a:false, n:10, thr:2}
-ARGV << '-h' if ARGV.size==0
+o = {q:false, n:100, thr:2}
+ARGV << "-h" if ARGV.size==0
 OptionParser.new do |opts|
-   opts.banner = "
+  opts.banner = "
 Subsamples the genomes in a set of Orthology Groups (OGs) and estimates the
 trend of core genome and pangenome sizes.
 
 Usage: #{$0} [options]"
-   opts.separator ""
-   opts.separator "Mandatory"
-   opts.on("-o", "--ogs FILE",
-      "Input file containing the precomputed OGs."){ |v| o[:ogs]=v }
-   opts.separator ""
-   opts.separator "Other Options"
-   opts.on("-n", "--replicates INT",
-      "Number of replicates to estimate. By default: #{o[:n]}."
-      ){ |v| o[:n]=v.to_i }
-   opts.on("-j", "--json FILE", "Output file in JSON format."){ |v| o[:json]=v }
-   opts.on("-t", "--tab FILE","Output file in tabular format."){ |v| o[:tab]=v }
-   opts.on("--threads INT",
-      "Children threads to spawn. By default: #{o[:thr]}."){ |v| o[:thr]=v.to_i}
-   opts.on("-s", "--summary FILE",
-      "Output file in tabular format with summary statistics."
-      ){ |v| o[:summ]=v }
-   opts.on("-a", "--auto", "Run completely quiertly (no STDERR or STDOUT)") do
-      o[:q] = true
-      o[:a] = true
-   end
-   opts.on("-q", "--quiet", "Run quietly (no STDERR output)."){ o[:q] = true }
-   opts.on("-h", "--help", "Display this screen.") do
-      puts opts
-      exit
-   end
-   opts.separator ""
+  opts.separator ""
+  opts.separator "Mandatory"
+  opts.on("-o", "--ogs FILE",
+    "Input file containing the precomputed OGs."){ |v| o[:ogs]=v }
+  opts.separator ""
+  opts.separator "Output Options"
+  opts.on("-s", "--summary FILE",
+    "Output file in tabular format with summary statistics."){ |v| o[:summ]=v }
+  opts.on("-t", "--tab FILE","Output file in tabular format."){ |v| o[:tab]=v }
+  opts.on("-j", "--json FILE", "Output file in JSON format."){ |v| o[:json]=v }
+  opts.separator ""
+  opts.separator "Other Options"
+  opts.on("-n", "--replicates INT",
+    "Number of replicates to estimate. By default: #{o[:n]}."
+    ){ |v| o[:n]=v.to_i }
+  opts.on("--threads INT",
+    "Children threads to spawn. By default: #{o[:thr]}."){ |v| o[:thr]=v.to_i}
+  opts.on("-q", "--quiet", "Run quietly (no STDERR output)."){ o[:q] = true }
+  opts.on("-h", "--help", "Display this screen.") do
+    puts opts
+    exit
+  end
+  opts.separator ""
 end.parse!
 abort "-o is mandatory" if o[:ogs].nil?
 
 ##### MAIN:
 begin
-   # Initialize the collection of OGs.
-   collection = OGCollection.new
-   
-   # Read the pre-computed OGs
-   $stderr.puts "Reading pre-computed OGs in '#{o[:ogs]}'." unless o[:q]
-   f = File.open(o[:ogs], "r")
-   h = f.gets.chomp.split /\t/
-   while ln = f.gets
-      collection << OG.new(h, ln.chomp.split(/\t/))
-   end
-   f.close
-   $stderr.puts " Loaded OGs: #{collection.ogs.length}." unless o[:q]
-   
-   # Generate subsamples
-   size = {core:[], pan:[]}
-   Dir.mktmpdir do |dir|
-      children = 0
-      (0 .. o[:n]-1).each do |i|
-	 fork do
-	    # Generate trajectory
-	    genomes = (0 .. Gene.genomes.length-1).to_a.shuffle
-	    core = []
-	    pan = []
-	    while not genomes.empty?
-	       core.unshift(collection.ogs.map do |og|
-		  (genomes - og.genomes).empty? ? 1 : 0
-	       end.inject(0,:+))
-	       pan.unshift(collection.ogs.map do |og|
-		  ((genomes - og.genomes).length < genomes.length) ? 1 : 0
-	       end.inject(0,:+))
-	       genomes.pop
-	    end
-	    # Store trajectory
-	    tfh = File.open("#{dir}/#{i}", "w")
-	    tfh.puts JSON.generate({core:core, pan:pan})
-	    tfh.close
-	 end # fork
-	 children += 1
-	 if children >= o[:thr]
-	    Process.wait
-	    children -= 1
-	 end
+  # Read the pre-computed OGs
+  $stderr.puts "Reading pre-computed OGs in '#{o[:ogs]}'." unless o[:q]
+  bool_a = []
+  genomes_n = nil
+  File.open(o[:ogs], "r") do |f|
+    h = f.gets.chomp.split "\t"
+    genomes_n = h.size
+    while ln = f.gets
+      bool_a << ln.chomp.split("\t").map{ |g| g!="-" }
+    end
+  end
+  $stderr.puts " Loaded OGs: #{bool_a.size}." unless o[:q]
+  bool_a_b = bool_a.map{ |og| og.map{ |g| g ? "1" : "0" }.join("").to_i(2) }
+
+  # Generate subsamples
+  size = {core:[], pan:[]}
+  Dir.mktmpdir do |dir|
+    children = 0
+    (0 .. o[:n]-1).each do |i|
+      fork do
+        # Generate trajectory
+        genomes = (0 .. genomes_n-1).to_a.shuffle
+        genomes_b = (2 ** genomes_n) - 1
+        core = []
+        pan = []
+        while not genomes.empty?
+          core.unshift 0
+          pan.unshift 0
+          bool_a_b.map! do |og|
+            r_og = og & genomes_b
+            if r_og>0
+              core[0] += 1 if r_og==genomes_b
+              pan[0]  += 1
+              og
+            else
+              nil
+            end
+          end
+          bool_a_b.compact!
+          genomes_b ^= 2 ** genomes.pop
+        end
+        abort "UNEXPECTED ERROR: Final genomes_b=#{genomes_b}." if genomes_b>0
+        # Store trajectory
+        File.open("#{dir}/#{i}", "w") do |tfh|
+          tfh.puts JSON.generate({core:core, pan:pan})
+        end
+      end # fork
+      children += 1
+      if children >= o[:thr]
+        Process.wait
+        children -= 1
       end
-      Process.waitall
-      # Recover trajectories
-      (0 .. o[:n]-1).each do |i|
-         s = JSON.parse(File.read("#{dir}/#{i}"), {:symbolize_names=>true})
-	 size[:core][i] = s[:core]
-	 size[:pan][i] = s[:pan]
+    end
+    Process.waitall
+    # Recover trajectories
+    (0 .. o[:n]-1).each do |i|
+      s = JSON.parse(File.read("#{dir}/#{i}"), {:symbolize_names=>true})
+      size[:core][i] = s[:core]
+      size[:pan][i] = s[:pan]
+    end
+  end # Dir.mktmpdir
+
+  # Show result
+  $stderr.puts "Generating reports." unless o[:q]
+
+  # Save results in JSON
+  unless o[:json].nil?
+    ofh = File.open(o[:json], "w")
+    ofh.puts JSON.pretty_generate(size)
+    ofh.close
+  end
+
+  # Save results in tab
+  unless o[:tab].nil?
+    ofh = File.open(o[:tab], "w")
+    ofh.puts (%w{replicate metric}+(1 .. genomes_n).to_a).join("\t")
+    (0 .. o[:n]-1).each do |i|
+      ofh.puts ([i+1,"core"] + size[:core][i]).join("\t")
+      ofh.puts ([i+1,"pan"] + size[:pan][i]).join("\t")
+    end
+    ofh.close
+  end
+
+  # Save summary results in tab
+  unless o[:summ].nil?
+    ofh = File.open(o[:summ], "w")
+    ofh.puts %w{genomes core_avg core_sd core_q1 core_q2 core_q3
+      pan_avg pan_sd pan_q1 pan_q2 pan_q3}.join("\t")
+    (0 .. genomes_n-1).each do |i|
+      res = [ i+1 ]
+      [:core, :pan].each do |met|
+        a = size[met].map{ |r| r[i] }.sort
+        avg = a.inject(0,:+).to_f / a.size
+        var = a.map{ |v| v**2 }.inject(0,:+).to_f/a.size - avg**2
+        sd = Math.sqrt(var)
+        q1 = a[ a.size*1/4 ]
+        q2 = a[ a.size*2/4 ]
+        q3 = a[ a.size*3/4 ]
+        res += [avg,sd,q1,q2,q3]
       end
-   end # Dir.mktmpdir
+      ofh.puts res.join("\t")
+    end
+    ofh.close
+  end
 
-   # Show result
-   $stderr.puts "Generating reports." unless o[:q]
-
-   # Save results in JSON
-   unless o[:json].nil?
-      ofh = File.open(o[:json], "w")
-      ofh.puts JSON.pretty_generate(size)
-      ofh.close
-   end
-
-   # Save results in tab
-   unless o[:tab].nil?
-      ofh = File.open(o[:tab], "w")
-      ofh.puts (%w{replicate metric}+(1 .. Gene.genomes.length).to_a).join("\t")
-      (0 .. o[:n]-1).each do |i|
-	 ofh.puts ([i+1,"core"] + size[:core][i]).join("\t")
-	 ofh.puts ([i+1,"pan"] + size[:pan][i]).join("\t")
-      end
-      ofh.close
-   end
-
-   # Save summary results in tab
-   unless o[:summ].nil?
-      ofh = File.open(o[:summ], "w")
-      ofh.puts %w{genomes core_avg core_sd core_q1 core_q2 core_q3
-	 pan_avg pan_sd pan_q1 pan_q2 pan_q3}.join("\t")
-      (0 .. Gene.genomes.length-1).each do |i|
-	 res = [ i+1 ]
-	 [:core, :pan].each do |met|
-	    a = size[met].map{ |r| r[i] }.sort
-	    avg = a.inject(0,:+).to_f / a.size
-	    var = a.map{ |v| v**2 }.inject(0,:+).to_f/a.size - avg**2
-	    sd = Math.sqrt(var)
-	    q1 = a[ a.size*1/4 ]
-	    q2 = a[ a.size*2/4 ]
-	    q3 = a[ a.size*3/4 ]
-	    res += [avg,sd,q1,q2,q3]
-	 end
-	 ofh.puts res.join("\t")
-      end
-      ofh.close
-   end
-
-   $stderr.puts "Done.\n" unless o[:q] 
+  $stderr.puts "Done.\n" unless o[:q] 
 rescue => err
-   $stderr.puts "Exception: #{err}\n\n"
-   err.backtrace.each { |l| $stderr.puts l + "\n" }
-   err
+  $stderr.puts "Exception: #{err}\n\n"
+  err.backtrace.each { |l| $stderr.puts l + "\n" }
+  err
 end
-
 
