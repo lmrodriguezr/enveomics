052dc65efae68e2566fbd9063b713a617297f65a
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2018-01-19 12:42:11 -0500

    New option BlastTab.recplot2.R --peaks-col
    Close #30

--- Scripts/BlastTab.recplot2.R
+++ Scripts/BlastTab.recplot2.R
@@ -1,40 +1,41 @@
 #!/usr/bin/env Rscript
-#
+
 # @author  Luis M. Rodriguez-R
-# @update  Jan-05-2016
 # @license artistic license 2.0
-#
 
 #= Load stuff
 suppressPackageStartupMessages(library(enveomics.R))
-args <- commandArgs(trailingOnly = F)  
+args <- commandArgs(trailingOnly = FALSE)
 enveomics_R <- file.path(dirname(
-   sub("^--file=", "", args[grep("^--file=", args)])),
-   "lib", "enveomics.R")
+  sub("^--file=", "", args[grep("^--file=", args)])),
+  "lib", "enveomics.R")
 
 #= Generate interface
 opt <- enve.cliopts(enve.recplot2,
-   file.path(enveomics_R, "man", "enve.recplot2.Rd"),
-   positional_arguments=c(1,4),
-   usage="usage: %prog [options] output.Rdata [output.pdf [width height]]",
-   mandatory=c("prefix"),
-   o_desc=list(pos.breaks="Breaks in the positions histogram.",
-     id.breaks="Breaks in the identity histogram.",
-     id.summary="Function summarizing the identity bins. By default: sum."),
-   p_desc=paste("","Produce recruitment plot objects provided that",
-     "BlastTab.catsbj.pl has been previously executed.", sep="\n\t"),
-   ignore=c("plot"),
-
-   defaults=c(id.metric="identity"))
+  file.path(enveomics_R, "man", "enve.recplot2.Rd"),
+  positional_arguments=c(1,4),
+  usage="usage: %prog [options] output.Rdata [output.pdf [width height]]",
+  mandatory=c("prefix"),
+  o_desc=list(pos.breaks="Breaks in the positions histogram.",
+    id.breaks="Breaks in the identity histogram.",
+    id.summary="Function summarizing the identity bins. By default: sum.",
+    peaks.col="Color of peaks, mandatory for peak-finding (e.g., darkred)."),
+  p_desc=paste("","Produce recruitment plot objects provided that",
+    "BlastTab.catsbj.pl has been previously executed.", sep="\n\t"),
+  ignore=c("plot"),
+  defaults=c(id.metric="identity", peaks.col=NA))
 
 #= Run it!
 if(length(opt$args)>1){
-   args = as.list(opt$args[-1])
-   for(i in 2:3) if(length(args)>=i) args[[i]] <- as.numeric(args[[i]])
-   do.call("pdf", args)
+  args = as.list(opt$args[-1])
+  for(i in 2:3) if(length(args)>=i) args[[i]] <- as.numeric(args[[i]])
+  do.call("pdf", args)
 }else{
-   opt$options[["plot"]] <- FALSE
+  opt$options[["plot"]] <- FALSE
 }
+pc <- opt$options[["peaks.col"]]
+if(!is.na(pc) && pc=="NA") opt$options[["peaks.col"]] <- NA
 rp <- do.call("enve.recplot2", opt$options)
 save(rp, file=opt$args[1])
 if(length(opt$args)>1) dev.off()
+
