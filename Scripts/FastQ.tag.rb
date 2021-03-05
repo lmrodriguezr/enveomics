b3341da14367f6bfc437c60c55b28680e262e43b
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2021-03-05 15:49:49 +0100

    FastQ.tag.rb supports (de)compression
    Also, code and doc style upgrade

--- Scripts/FastQ.tag.rb
+++ Scripts/FastQ.tag.rb
@@ -1,63 +1,68 @@
 #!/usr/bin/env ruby
 
-#
-# @author: Luis M. Rodriguez-R
-# @update: Feb-06-2015
-# @license: artistic license 2.0
-#
+# frozen_string_literal: true
 
-require 'optparse'
+$:.push File.expand_path('../lib', __FILE__)
+require 'enveomics_rb/enveomics'
+$VERSION = 1.0
 
-o = {:q=>FALSE, :p=>"", :s=>""}
-ARGV << '-h' if ARGV.size==0
+o = { q: false, p: '', s: '' }
 OptionParser.new do |opts|
-   opts.banner = "
-Generates easy-to-parse tagged reads from FastQ files.
+  opts.version = $VERSION
+  Enveomics.opt_banner(
+    opts, 'Generates easy-to-parse tagged reads from FastQ files',
+    "#{File.basename($0)} -i in.fasta -o out.fasta [options]"
+  )
 
-Usage: #{$0} [options]"
-   opts.separator ""
-   opts.separator "Mandatory"
-   opts.on("-i", "--in FILE", "Path to the FastQ file containing the sequences."){ |v| o[:in] = v }
-   opts.on("-o", "--out FILE", "Path to the FastQ to create."){ |v| o[:out] = v }
-   opts.separator ""
-   opts.separator "ID options"
-   opts.on("-p", "--prefix STR", "Prefix to use in all IDs."){ |v| o[:p] = v }
-   opts.on("-s", "--suffix STR", "Suffix to use in all IDs."){ |v| o[:s] = v }
-   opts.separator ""
-   opts.separator "Other Options"
-   opts.on("-q", "--quiet", "Run quietly (no STDERR output)"){ o[:q] = TRUE }
-   opts.on("-h", "--help", "Display this screen") do
-      puts opts
-      exit
-   end
-   opts.separator ""
+  opts.separator 'Mandatory'
+  opts.on(
+    '-i', '--in FILE',
+    'Path to the FastQ file containing the sequences',
+    'Supports compression with .gz extension, use - for STDIN'
+  ) { |v| o[:in] = v }
+  opts.on(
+    '-o', '--out FILE', 'Path to the FastQ to create',
+    'Supports compression with .gz extension, use - for STDOUT'
+  ) { |v| o[:out] = v }
+  opts.separator ''
+  opts.separator 'ID options'
+  opts.on('-p', '--prefix STR', 'Prefix to use in all IDs') { |v| o[:p] = v }
+  opts.on('-s', '--suffix STR', 'Suffix to use in all IDs') { |v| o[:s] = v }
+  opts.separator ''
+  opts.separator 'Other Options'
+  opts.on('-q', '--quiet', 'Run quietly (no STDERR output)') { o[:q] = true }
+  opts.on('-h', '--help', 'Display this screen') { puts opts ; exit }
+  opts.separator ''
 end.parse!
-abort "-i is mandatory" if o[:in].nil?
-abort "-o is mandatory" if o[:out].nil?
-   
-begin
-   ifh = File.open(o[:in], 'r');
-   ofh = File.open(o[:out], 'w');
-   i=0
-   while ln=ifh.gets
-      ln.chomp!
-      if $.%4==1 and not /^@/.match(ln).nil?
-	 i+=1
-	 ofh.puts "@#{o[:p]}#{i}#{o[:s]}"
-      elsif $.%4==2 or $.%4==0
-         ofh.puts ln
-      elsif $.%4==3 and not /^\+/.match(ln).nil?
-         ofh.puts "+"
-      else
-         abort "Impossible to parse line #{$.}: #{ln}.\n"
-      end
-   end
-   ifh.close
-   ofh.close
-rescue => err
-   $stderr.puts "Exception: #{err}\n\n"
-   err.backtrace.each { |l| $stderr.puts l + "\n" }
-   err
-end
 
+raise Enveomics::OptionError.new('-i is mandatory') if o[:in].nil?
+raise Enveomics::OptionError.new('-o is mandatory') if o[:out].nil?
+
+begin
+  ifh = reader(o[:in])
+  ofh = writer(o[:out])
+  i = 0
+  while ln = ifh.gets
+    ln.chomp!
+    case $. % 4
+    when 1
+      ln =~ /^@/ or
+        raise Enveomics::ParseError.new("Cannot parse line #{$.}: #{ln}")
+      i += 1
+      ofh.puts "@#{o[:p]}#{i}#{o[:s]}"
+    when 3
+      ln =~ /^\+/ or
+        raise Enveomics::ParseError.new("Cannot parse line #{$.}: #{ln}")
+      ofh.puts '+'
+    else
+      ofh.puts ln
+    end
+  end
+  ifh.close
+  ofh.close
+rescue => err
+  $stderr.puts "Exception: #{err}\n\n"
+  err.backtrace.each { |l| $stderr.puts l + "\n" }
+  err
+end
 
