682908569d223719016bb8b2a7171db8b99728c7
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2022-03-09 16:13:31 +0100

    Add BedGraph.tar.rb options
    - Centralized code (adding `--version`)
    - New option `--out`
    - Option `--input` adds gz/STDIN support
    - New option `--name`
    - New gzipped bedgraph to run tests

--- Scripts/BedGraph.tad.rb
+++ Scripts/BedGraph.tad.rb
@@ -1,40 +1,64 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
+$VERSION = 1.0
+$:.push File.expand_path('../lib', __FILE__)
+require 'enveomics_rb/enveomics'
 
-o = {range: 0.5, perseq: false, length: false}
-ARGV << '-h' if ARGV.empty?
-OptionParser.new do |opt|
-  opt.banner = "
-  Estimates the truncated average sequencing depth (TAD) from a BedGraph file.
+o = { range: 0.5, perseq: false, length: false, o: '-' }
+OptionParser.new do |opts|
+  opts.version = $VERSION
+  banner = <<~BANNER
+    Estimates the truncated average sequencing depth (TAD) from a BedGraph file
 
-  IMPORTANT: This script doesn't consider zero-coverage positions if missing
-  from the file. If you produce your BedGraph file with bedtools genomecov and
-  want to consider zero-coverage position, be sure to use -bga (not -bg).
+    IMPORTANT: This script doesn't consider zero-coverage positions if missing
+    from the file. If you produce your BedGraph file with bedtools genomecov and
+    want to consider zero-coverage position, be sure to use -bga (not -bg).
+  BANNER
+  Enveomics.opt_banner(opts, banner, "#{File.basename($0)} [options]")
 
-  Usage: #{$0} [options]"
-  opt.separator ''
-  opt.on('-i', '--input PATH',
-    'Input BedGraph file (mandatory).'){ |v| o[:i]=v }
-  opt.on('-r', '--range FLOAT',
-    'Central range to consider, between 0 and 1.',
-    "By default: #{o[:range]} (inter-quartile range)."
-    ){ |v| o[:range]=v.to_f }
-  opt.on('-s', '--per-seq',
-    'Calculate averages per reference sequence, not total.',
-    'Assumes a sorted BedGraph file.'
-    ){ |v| o[:perseq] = v }
-  opt.on('-l', '--length',
-    'Add sequence length to the output.'){ |v| o[:length] = v }
-  opt.on('-b', '--breadth',
-    'Add sequencing breadth to the output.'){ |v| o[:breadth] = v }
-  opt.on('-h', '--help', 'Display this screen.') do
-    puts opt
+  opts.separator 'Mandatory'
+  opts.on(
+    '-i', '--input PATH',
+    'Input BedGraph file',
+    'Supports compression with .gz extension, use - for STDIN'
+  ) { |v| o[:i] = v }
+
+  opts.separator ''
+  opts.separator 'Other Options'
+  opts.on(
+    '-o', '--out PATH',
+    'Output tab-delimited values (by default, STDOUT)',
+    'Supports compression with .gz extension, use - for STDOUT'
+  ) { |v| o[:o] = v }
+  opts.on(
+    '-r', '--range FLOAT', Float,
+    'Central range to consider, between 0 and 1',
+    "By default: #{o[:range]} (inter-quartile range)"
+  ) { |v| o[:range] = v }
+  opts.on(
+    '-n', '--name STRING',
+    'Name (ID) of the sequence (added as first column)'
+  ) { |v| o[:name] = v }
+  opts.on(
+    '-s', '--per-seq',
+    'Calculate averages per reference sequence, not total',
+    'Assumes a sorted BedGraph file'
+  ) { |v| o[:perseq] = v }
+  opts.on(
+    '-l', '--length',
+    'Add sequence length to the output'
+  ) { |v| o[:length] = v }
+  opts.on(
+    '-b', '--breadth',
+    'Add sequencing breadth to the output'
+  ) { |v| o[:breadth] = v }
+  opts.on('-h', '--help', 'Display this screen') do
+    puts opts
     exit
   end
-  opt.separator ''
+  opts.separator ''
 end.parse!
-abort '-i is mandatory.' if o[:i].nil?
+raise Enveomics::OptionError.new('-i is mandatory') if o[:i].nil?
 
 ##
 # Pad an array to include all index values up to +r+ entries:
@@ -79,32 +103,36 @@ def report(sq, d, ln, o)
 
   # Report
   y.unshift(sq) if o[:perseq]
+  y.unshift(o[:name]) if o[:name]
   y << ln if o[:length]
   y << (ln - zeroes).to_f / ln if o[:breadth]
-  puts y.join("\t")
+  y.join("\t")
 end
 
 # Read BedGraph
 d  = [] # [ depth => count ]
 ln = 0
 pre_sq = nil
-File.open(o[:i], 'r') do |ifh|
-  ifh.each_line do |i|
-    next if i =~ /^#/
-    r  = i.chomp.split("\t")
-    sq = r.shift # Contig ID
-    if o[:perseq] && !pre_sq.nil? && pre_sq != sq
-      report(pre_sq, d, ln, o)
-      d  = []
-      ln = 0
-    end
-    r.map! { |j| j.to_i } # From, To, Depth
-    l = r[1] - r[0] # Window length: To - From
-    d[ r[2] ] ||= 0
-    d[ r[2] ]  += l # Add this "l" positions with depth "Depth"
-    ln += l
-    pre_sq = sq
+ifh = reader(o[:i])
+ofh = writer(o[:o])
+ifh.each_line do |i|
+  next if i =~ /^#/
+  r  = i.chomp.split("\t")
+  sq = r.shift # Contig ID
+  if o[:perseq] && !pre_sq.nil? && pre_sq != sq
+    ofh.puts(report(pre_sq, d, ln, o))
+    d  = []
+    ln = 0
   end
+  r.map! { |j| j.to_i } # From, To, Depth
+  l = r[1] - r[0] # Window length: To - From
+  d[ r[2] ] ||= 0
+  d[ r[2] ]  += l # Add this "l" positions with depth "Depth"
+  ln += l
+  pre_sq = sq
 end
-report(pre_sq, d, ln, o)
+ofh.puts(report(pre_sq, d, ln, o))
+
+ifh.close
+ofh.close
 
