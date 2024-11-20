596e13c9fdc7cd075e77d5d6f3a76281faa62f81
Luis M. Rodriguez-R <lmrodriguezr@gmail.com>
2024-11-20 16:47:53 +0100

    Update FastA.tag.rb

--- Scripts/FastA.tag.rb
+++ Scripts/FastA.tag.rb
@@ -1,61 +1,77 @@
 #!/usr/bin/env ruby
 
-# @author  Luis M. Rodriguez-R
-# @license artistic license 2.0
+# frozen_string_literal: true
 
-require 'optparse'
+$:.push File.expand_path('../lib', __FILE__)
+require 'enveomics_rb/enveomics'
+$VERSION = 1.0
 
-o = {q: false, p: '', s: '', d: false}
-ARGV << '-h' if ARGV.size==0
+o = { q: false, p: '', s: '', d: false }
+
 OptionParser.new do |opts|
-  opts.banner = "
-Generates easy-to-parse tagged reads from FastA files.
+  opts.version = $VERSION
+  Enveomics.opt_banner(
+    opts, 'Generates easy-to-parse tagged reads from FastA files',
+    "#{File.basename($0)} -i in.fasta -o out.fasta [options]"
+  )
 
-Usage: #{$0} [options]"
-  opts.separator ''
   opts.separator 'Mandatory'
-  opts.on('-i', '--in FILE',
-    'Path to the FastA file containing the sequences.'){ |v| o[:in] = v }
-  opts.on('-o', '--out FILE',
-    'Path to the FastA to create.'){ |v| o[:out] = v }
+  opts.on(
+    '-i', '--in FILE',
+    'Path to the FastA file containing the sequences',
+    'Supports compression with .gz extension, use - for STDIN'
+  ) { |v| o[:in] = v }
+  opts.on(
+    '-o', '--out FILE',
+    'Path to the FastA to create',
+    'Supports compression with .gz extension, use - for STDOUT'
+  ) { |v| o[:out] = v }
+
   opts.separator ''
   opts.separator 'ID options'
-  opts.on('-p', '--prefix STR', 'Prefix to use in all IDs.'){ |v| o[:p] = v }
-  opts.on('-s', '--suffix STR', 'Suffix to use in all IDs.'){ |v| o[:s] = v }
-  opts.on('-d', '--defline',
-    'Keep the original defline after a space.'){ o[:d] = true }
-  opts.on('-l', '--list FILE',
-    'Reads a list of IDS.'){ |v| o[:l] = v }
+  opts.on('-p', '--prefix STR', 'Prefix to use in all IDs') { |v| o[:p] = v }
+  opts.on('-s', '--suffix STR', 'Suffix to use in all IDs') { |v| o[:s] = v }
+  opts.on(
+    '-d', '--defline', 'Keep the original defline after a space'
+  ) { o[:d] = true }
+  opts.on(
+    '-l', '--list FILE', 'Reads a list of IDS',
+    'Supports compression with .gz extension, use - for STDIN'
+  ) { |v| o[:l] = v }
+
   opts.separator ''
   opts.separator 'Other Options'
-  opts.on('-q', '--quiet', 'Run quietly (no STDERR output)'){ o[:q] = true }
-  opts.on('-h', '--help', 'Display this screen') do
-    puts opts
-    exit
-  end
+  opts.on('-q', '--quiet', 'Run quietly (no STDERR output)') { o[:q] = true }
+  opts.on('-h', '--help', 'Display this screen') { puts opts; exit }
   opts.separator ''
 end.parse!
-abort '-i is mandatory' if o[:in].nil?
-abort '-o is mandatory' if o[:out].nil?
+
+raise Enveomics::OptionError.new('-i is mandatory') if o[:in].nil?
+raise Enveomics::OptionError.new('-o is mandatory') if o[:out].nil?
    
 begin
-  list = o[:l].nil? ? nil :
-    File.readlines(o[:l]).map{ |i| i.chomp.gsub(/^>/, '') }
-  ofh = File.open(o[:out], 'w')
+  list = nil
+  unless o[:l].nil?
+    lfh = reader(o[:l])
+    list = lfh.map { |i| i.chomp.gsub(/^>/, '') }
+    lfh.close
+  end
+  
+  ofh = writer(o[:out])
   i = 0
-  File.open(o[:in], 'r') do |ifh|
-    ifh.each do |ln|
-      ln.chomp!
-      next if ln =~ /^;/
-      unless /^>/.match(ln).nil?
-        i += 1
-        new_id = o[:l].nil? ? i : list.shift
-        ofh.puts ">#{o[:p]}#{new_id}#{o[:s]}#{o[:d]?" #{ln[1, ln.size-1]}":''}"
-      else
-        ofh.puts ln
-      end
+  ifh = reader(o[:in])
+  ifh.each do |ln|
+    ln.chomp!
+    next if ln =~ /^;/
+    unless /^>/.match(ln).nil?
+      i += 1
+      new_id = o[:l].nil? ? i : list.shift
+      ofh.puts ">#{o[:p]}#{new_id}#{o[:s]}#{o[:d]?" #{ln[1, ln.size-1]}":''}"
+    else
+      ofh.puts ln
     end
   end
+  ifh.close
   ofh.close
 rescue => err
   $stderr.puts "Exception: #{err}\n\n"
